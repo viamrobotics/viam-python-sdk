c056fcce0aee4e5beb4e7f1c1c0e798849cbadae
Naveed Jooma <naveed@viam.com>
2022-01-26 18:07:58 -0500

    Adding an init file

--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
-import setuptools
+from setuptools import setup, find_packages
+import pathlib
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+here = pathlib.Path(__file__).parent.resolve()
 
-setuptools.setup(
-    name='toolbox',
-    version='0.0.3',
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
-    description='Testing installation of Package',
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='viam',
+    version='0.0.1',
+    author='Viam',
+    author_email='naveed@viam.com',
+    description='Viam Python SDK to connect to robots running the Viam RDK',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/Muls/toolbox',
+    long_description_content_type='text/markdown',
+    url='https://github.com/viamrobotics/python-sdk',
     project_urls={
-        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+        'Bug Tracker': 'https://github.com/viamrobotics/python-sdk/issues',
     },
-    license='MIT',
+    keywords='robotics, robots',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     packages=['viam'],
+    python_requires='>=3.6, <4',
     install_requires=['grpclib'],
 )
