62ad5a7be6b8f29dee942f031bc39543584a7391
Naveed Jooma <naveed@viam.com>
2022-01-26 17:50:52 -0500

    Fake setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-from setuptools import setup, find_packages
-import pathlib
+import setuptools
 
-here = pathlib.Path(__file__).parent.resolve()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-long_description = (here / 'README.md').read_text(encoding='utf-8')
-
-setup(
+setuptools.setup(
     name='viam-python-sdk',
     version='0.0.1',
     author='Viam',
     author_email='naveed@viam.com',
-    description='Viam Python SDK to connect to robots running the Viam RDK',
+    description='Testing installation of Package',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url='https://github.com/viamrobotics/python-sdk',
     project_urls={
-        'Bug Tracker': 'https://github.com/viamrobotics/python-sdk/issues',
+        "Bug Tracker": "https://github.com/viamrobotics/python-sdk/issues"
     },
-    keywords='robotics, robots',
     packages=['viam'],
-    python_requires='>=3.6, <4',
     install_requires=['grpclib'],
 )
