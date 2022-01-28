4c9a40af8b1b84601a61a8ddb63e0ff3b3ad2dd7
Naveed Jooma <naveed@viam.com>
2022-01-28 16:29:47 -0500

    Change import

--- viam/proto.py
+++ viam/proto.py
@@ -1,4 +1,4 @@
-import gen
+import viam.gen
 
-from gen.proto.api.v1 import (robot_grpc, robot_pb2)
-from gen.proto.rpc.v1 import (auth_grpc, auth_pb2)
+from viam.gen.proto.api.v1 import (robot_grpc, robot_pb2)
+from viam.gen.proto.rpc.v1 import (auth_grpc, auth_pb2)
