49543e9b8bc143dd18585b64688a471322701fbf
viambot <viambot@users.noreply.github.com>
2022-02-22 17:04:20 +0000

    [WORKFLOW] Updating protos from viamrobotics/rdk, commit: e000035c357d61a92038f4d9d1f837e0028a7130

--- viam/gen/proto/api/component/sensor/v1/sensor_pb2.py
+++ viam/gen/proto/api/component/sensor/v1/sensor_pb2.py
@@ -7,7 +7,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*proto/api/component/sensor/v1/sensor.proto\x12\x1dproto.api.component.sensor.v1\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto"(\n\x12GetReadingsRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name"I\n\x13GetReadingsResponse\x122\n\x08readings\x18\x01 \x03(\x0b2\x16.google.protobuf.ValueR\x08readings2\xb8\x01\n\rSensorService\x12\xa6\x01\n\x0bGetReadings\x121.proto.api.component.sensor.v1.GetReadingsRequest\x1a2.proto.api.component.sensor.v1.GetReadingsResponse"0\x82\xd3\xe4\x93\x02*\x12(/api/v1/component/sensor/{name}/readingsBM\n#com.viam.rdk.proto.api.component.v1Z&go.viam.com/rdk/proto/api/component/v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*proto/api/component/sensor/v1/sensor.proto\x12\x1dproto.api.component.sensor.v1\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto"(\n\x12GetReadingsRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name"I\n\x13GetReadingsResponse\x122\n\x08readings\x18\x01 \x03(\x0b2\x16.google.protobuf.ValueR\x08readings2\xb8\x01\n\rSensorService\x12\xa6\x01\n\x0bGetReadings\x121.proto.api.component.sensor.v1.GetReadingsRequest\x1a2.proto.api.component.sensor.v1.GetReadingsResponse"0\x82\xd3\xe4\x93\x02*\x12(/api/v1/component/sensor/{name}/readingsB[\n*com.viam.rdk.proto.api.component.sensor.v1Z-go.viam.com/rdk/proto/api/component/sensor/v1b\x06proto3')
 _GETREADINGSREQUEST = DESCRIPTOR.message_types_by_name['GetReadingsRequest']
 _GETREADINGSRESPONSE = DESCRIPTOR.message_types_by_name['GetReadingsResponse']
 GetReadingsRequest = _reflection.GeneratedProtocolMessageType('GetReadingsRequest', (_message.Message,), {'DESCRIPTOR': _GETREADINGSREQUEST, '__module__': 'proto.api.component.sensor.v1.sensor_pb2'})
@@ -17,7 +17,7 @@ _sym_db.RegisterMessage(GetReadingsResponse)
 _SENSORSERVICE = DESCRIPTOR.services_by_name['SensorService']
 if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
-    DESCRIPTOR._serialized_options = b'\n#com.viam.rdk.proto.api.component.v1Z&go.viam.com/rdk/proto/api/component/v1'
+    DESCRIPTOR._serialized_options = b'\n*com.viam.rdk.proto.api.component.sensor.v1Z-go.viam.com/rdk/proto/api/component/sensor/v1'
     _SENSORSERVICE.methods_by_name['GetReadings']._options = None
     _SENSORSERVICE.methods_by_name['GetReadings']._serialized_options = b'\x82\xd3\xe4\x93\x02*\x12(/api/v1/component/sensor/{name}/readings'
     _GETREADINGSREQUEST._serialized_start = 137
--- viam/gen/proto/api/service/metadata/v1/metadata_pb2.py
+++ viam/gen/proto/api/service/metadata/v1/metadata_pb2.py
@@ -9,7 +9,7 @@ from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 from google.protobuf import duration_pb2 as google_dot_protobuf_dot_duration__pb2
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from google.api import httpbody_pb2 as google_dot_api_dot_httpbody__pb2
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n,proto/api/service/metadata/v1/metadata.proto\x12\x1dproto.api.service.metadata.v1\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1egoogle/protobuf/duration.proto\x1a\x1cgoogle/api/annotations.proto\x1a\x19google/api/httpbody.proto"\x12\n\x10ResourcesRequest"\x82\x01\n\x0cResourceName\x12\x12\n\x04uuid\x18\x01 \x01(\tR\x04uuid\x12\x1c\n\tnamespace\x18\x02 \x01(\tR\tnamespace\x12\x12\n\x04type\x18\x03 \x01(\tR\x04type\x12\x18\n\x07subtype\x18\x04 \x01(\tR\x07subtype\x12\x12\n\x04name\x18\x05 \x01(\tR\x04name"^\n\x11ResourcesResponse\x12I\n\tresources\x18\x01 \x03(\x0b2+.proto.api.service.metadata.v1.ResourceNameR\tresources2\xa4\x01\n\x0fMetadataService\x12\x90\x01\n\tResources\x12/.proto.api.service.metadata.v1.ResourcesRequest\x1a0.proto.api.service.metadata.v1.ResourcesResponse" \x82\xd3\xe4\x93\x02\x1a\x12\x18/api/v1/service/metadataBI\n!com.viam.rdk.proto.api.service.v1Z$go.viam.com/rdk/proto/api/service/v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n,proto/api/service/metadata/v1/metadata.proto\x12\x1dproto.api.service.metadata.v1\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1egoogle/protobuf/duration.proto\x1a\x1cgoogle/api/annotations.proto\x1a\x19google/api/httpbody.proto"\x12\n\x10ResourcesRequest"\x82\x01\n\x0cResourceName\x12\x12\n\x04uuid\x18\x01 \x01(\tR\x04uuid\x12\x1c\n\tnamespace\x18\x02 \x01(\tR\tnamespace\x12\x12\n\x04type\x18\x03 \x01(\tR\x04type\x12\x18\n\x07subtype\x18\x04 \x01(\tR\x07subtype\x12\x12\n\x04name\x18\x05 \x01(\tR\x04name"^\n\x11ResourcesResponse\x12I\n\tresources\x18\x01 \x03(\x0b2+.proto.api.service.metadata.v1.ResourceNameR\tresources2\xa4\x01\n\x0fMetadataService\x12\x90\x01\n\tResources\x12/.proto.api.service.metadata.v1.ResourcesRequest\x1a0.proto.api.service.metadata.v1.ResourcesResponse" \x82\xd3\xe4\x93\x02\x1a\x12\x18/api/v1/service/metadataB[\n*com.viam.rdk.proto.api.service.metadata.v1Z-go.viam.com/rdk/proto/api/service/metadata/v1b\x06proto3')
 _RESOURCESREQUEST = DESCRIPTOR.message_types_by_name['ResourcesRequest']
 _RESOURCENAME = DESCRIPTOR.message_types_by_name['ResourceName']
 _RESOURCESRESPONSE = DESCRIPTOR.message_types_by_name['ResourcesResponse']
@@ -22,7 +22,7 @@ _sym_db.RegisterMessage(ResourcesResponse)
 _METADATASERVICE = DESCRIPTOR.services_by_name['MetadataService']
 if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
-    DESCRIPTOR._serialized_options = b'\n!com.viam.rdk.proto.api.service.v1Z$go.viam.com/rdk/proto/api/service/v1'
+    DESCRIPTOR._serialized_options = b'\n*com.viam.rdk.proto.api.service.metadata.v1Z-go.viam.com/rdk/proto/api/service/metadata/v1'
     _METADATASERVICE.methods_by_name['Resources']._options = None
     _METADATASERVICE.methods_by_name['Resources']._serialized_options = b'\x82\xd3\xe4\x93\x02\x1a\x12\x18/api/v1/service/metadata'
     _RESOURCESREQUEST._serialized_start = 198
