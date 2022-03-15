a7ce0b11a82884d4480a62a8b86a5933ad45eb44
Naveed Jooma <naveed@viam.com>
2022-03-15 16:44:09 -0400

    Remove unused robot proto defs

--- viam/robot/service.py
+++ viam/robot/service.py
@@ -1,40 +1,15 @@
 from grpclib.server import Stream
 from viam.components.service_base import ComponentServiceBase
-from viam.proto.api.robot import (ConfigRequest, ConfigResponse,
-                                  DoActionRequest, DoActionResponse,
-                                  ExecuteFunctionRequest,
-                                  ExecuteFunctionResponse,
-                                  ExecuteSourceRequest, ExecuteSourceResponse,
+from viam.proto.api.robot import (DoActionRequest, DoActionResponse,
                                   ResourceRunCommandRequest,
                                   ResourceRunCommandResponse, RobotServiceBase)
 
 
 class RobotService(RobotServiceBase, ComponentServiceBase):
 
-    async def Config(
-        self,
-        stream: Stream[ConfigRequest, ConfigResponse]
-    ) -> None:
-        request = await stream.recv_message()
-        assert request is not None
-        response = ConfigResponse()
-        await stream.send_message(response)
-
     async def DoAction(
         self,
         stream: Stream[DoActionRequest, DoActionResponse]
-    ) -> None:
-        pass
-
-    async def ExecuteFunction(
-        self,
-        stream: Stream[ExecuteFunctionRequest, ExecuteFunctionResponse]
-    ) -> None:
-        pass
-
-    async def ExecuteSource(
-        self,
-        stream: Stream[ExecuteSourceRequest, ExecuteSourceResponse]
     ) -> None:
         pass
 
