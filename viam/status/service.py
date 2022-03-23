5493f58d862ca2a4ff97bde8d1e9c7b867748584
Naveed Jooma <naveed@viam.com>
2022-03-23 16:41:41 -0400

    Add services to status, enable filtering (#44)

--- viam/status/service.py
+++ viam/status/service.py
@@ -1,15 +1,18 @@
 import asyncio
-from typing import List
+from typing import Iterable, List
 
 from google.protobuf.struct_pb2 import Struct
 from grpclib.server import Stream
 from viam.components.arm import Arm
 from viam.components.base import Base
 from viam.components.board import Board
+from viam.components.camera import Camera
+from viam.components.pose_tracker import PoseTracker
 from viam.components.motor import Motor
 from viam.components.sensor import Sensor
 from viam.components.service_base import ComponentServiceBase
 from viam.components.servo import Servo
+from viam.proto.api.common import ResourceName
 from viam.proto.api.component.arm import Status as ArmStatus
 from viam.proto.api.component.motor import Status as MotorStatus
 from viam.proto.api.component.servo import Status as ServoStatus
@@ -22,8 +25,25 @@ from viam.utils import message_to_struct, resource_name_for_component_type
 
 class StatusService(StatusServiceBase, ComponentServiceBase):
 
-    async def _generate_status(self) -> List[Status]:
-        statuses: List[Status] = []
+    async def _generate_status(self, resource_names: Iterable[ResourceName]) -> List[Status]:
+        statuses: List[Status] = [
+            Status(
+                name=ResourceName(
+                    namespace='rdk',
+                    type='service',
+                    subtype='status',
+                ),
+                status=Struct()
+            ),
+            Status(
+                name=ResourceName(
+                    namespace='rdk',
+                    type='service',
+                    subtype='object_segmentation',
+                ),
+                status=Struct()
+            )
+        ]
 
         for component in self.manager.components.values():
             if isinstance(component, Arm):
@@ -51,6 +71,12 @@ class StatusService(StatusServiceBase, ComponentServiceBase):
                     status=message_to_struct(await component.status())
                 )
                 statuses.append(status)
+            if isinstance(component, Camera):
+                status = Status(
+                    name=resource_name_for_component_type(component, Camera),
+                    status=Struct()
+                )
+                statuses.append(status)
             if isinstance(component, Motor):
                 s = MotorStatus()
                 s.is_on = await component.is_powered()
@@ -61,6 +87,13 @@ class StatusService(StatusServiceBase, ComponentServiceBase):
                 status = Status(
                     name=resource_name_for_component_type(component, Motor),
                     status=as_struct
+                )
+                statuses.append(status)
+            if isinstance(component, PoseTracker):
+                status = Status(
+                    name=resource_name_for_component_type(
+                        component, PoseTracker),
+                    status=Struct()
                 )
                 statuses.append(status)
             if isinstance(component, Sensor):
@@ -79,6 +112,8 @@ class StatusService(StatusServiceBase, ComponentServiceBase):
                 )
                 statuses.append(status)
 
+        if resource_names:
+            statuses = [s for s in statuses if s.name in resource_names]
         return statuses
 
     async def GetStatus(
@@ -87,7 +122,7 @@ class StatusService(StatusServiceBase, ComponentServiceBase):
     ) -> None:
         request = await stream.recv_message()
         assert request is not None
-        status = await self._generate_status()
+        status = await self._generate_status(request.resource_names)
         response = GetStatusResponse(status=status)
         await stream.send_message(response)
 
@@ -102,7 +137,7 @@ class StatusService(StatusServiceBase, ComponentServiceBase):
         if every > 0:
             interval = every
         while True:
-            status = await self._generate_status()
+            status = await self._generate_status(request.resource_names)
             response = StreamStatusResponse(status=status)
             await stream.send_message(response)
             await asyncio.sleep(interval)
