a62687966be08a505f68ee1e2b858409c7269d9c
Naveed Jooma <naveedjooma@gmail.com>
2022-03-30 18:40:38 -0400

    Adding all in better imports

--- etc/generate_proto_import.py
+++ etc/generate_proto_import.py
@@ -138,6 +138,10 @@ def build_dirs(root: str, package: str, modules: List[str]):
                 )
                 f.write('    %s\n' % (',\n    '.join(cls)))
                 f.write(')\n')
+            f.write('\n__all__ = [\n')
+            for (imp, cls) in classes.items():
+                f.write('    %s\n' % (',\n    '.join([f"'{c}'" for c in cls])))
+            f.write(']\n')
 
 
 def run():
