fb733f0c6a6d1d67143c339f49c52508c95125e5
Naveed Jooma <naveed@viam.com>
2022-04-04 18:28:46 -0400

    Update requirements

--- viam/gen/proto/api/common/v1/common_pb2.pyi
+++ viam/gen/proto/api/common/v1/common_pb2.pyi
@@ -92,6 +92,7 @@ class AnalogStatus(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     VALUE_FIELD_NUMBER: builtins.int
     value: builtins.int = ...
+    "Current value of the analog reader of a robot's board"
 
     def __init__(self, *, value: builtins.int=...) -> None:
         ...
@@ -104,6 +105,7 @@ class DigitalInterruptStatus(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     VALUE_FIELD_NUMBER: builtins.int
     value: builtins.int = ...
+    "Current value of the digital interrupt of a robot's board"
 
     def __init__(self, *, value: builtins.int=...) -> None:
         ...
@@ -113,6 +115,15 @@ class DigitalInterruptStatus(google.protobuf.message.Message):
 global___DigitalInterruptStatus = DigitalInterruptStatus
 
 class Pose(google.protobuf.message.Message):
+    """Pose is a combination of location and orientation.
+    Location is expressed as distance which is represented by x , y, z coordinates. Orientation is expressed as an orientation vector which 
+    is represented by o_x, o_y, o_z and theta. The o_x, o_y, o_z coordinates represent the point on the cartesian unit sphere that the end of
+    the arm is pointing to (with the origin as reference). That unit vector forms an axis around which theta rotates. This means that 
+    incrementing / decrementing theta will perform an inline rotation of the end effector. 
+    Theta is defined as rotation between two planes: the first being defined by the origin, the point (0,0,1), and the rx, ry, rz point, and the 
+    second being defined by the origin, the rx, ry, rz point and the local Z axis. Therefore, if theta is kept at zero as the north/south pole 
+    is circled, the Roll will correct itself to remain in-line.
+    """
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     X_FIELD_NUMBER: builtins.int
     Y_FIELD_NUMBER: builtins.int
@@ -122,14 +133,19 @@ class Pose(google.protobuf.message.Message):
     O_Z_FIELD_NUMBER: builtins.int
     THETA_FIELD_NUMBER: builtins.int
     x: builtins.float = ...
-    'millimeters of the end effector from the base'
+    'millimeters from the origin'
     y: builtins.float = ...
+    'millimeters from the origin'
     z: builtins.float = ...
+    'millimeters from the origin'
     o_x: builtins.float = ...
-    'ox, oy, oz, theta represents an orientation vector\n    Structured similarly to an angle axis, an orientation vector works differently. Rather than representing an orientation\n    with an arbitrary axis and a rotation around it from an origin, an orientation vector represents orientation\n    such that the ox/oy/oz components represent the point on the cartesian unit sphere at which your end effector is pointing\n    from the origin, and that unit vector forms an axis around which theta rotates. This means that incrementing/decrementing\n    theta will perform an in-line rotation of the end effector.\n    Theta is defined as rotation between two planes: the plane defined by the origin, the point (0,0,1), and the rx,ry,rz\n    point, and the plane defined by the origin, the rx,ry,rz point, and the new local Z axis. So if theta is kept at\n    zero as the north/south pole is circled, the Roll will correct itself to remain in-line.\n    Theta in pb.Pose should be degrees. It will be converted to radians in the internal OrientationVec.\n    '
+    'z component of a vector defining axis of rotation'
     o_y: builtins.float = ...
+    'x component of a vector defining axis of rotation'
     o_z: builtins.float = ...
+    'y component of a vector defining axis of rotation'
     theta: builtins.float = ...
+    'degrees'
 
     def __init__(self, *, x: builtins.float=..., y: builtins.float=..., z: builtins.float=..., o_x: builtins.float=..., o_y: builtins.float=..., o_z: builtins.float=..., theta: builtins.float=...) -> None:
         ...
@@ -139,6 +155,7 @@ class Pose(google.protobuf.message.Message):
 global___Pose = Pose
 
 class PoseInFrame(google.protobuf.message.Message):
+    """PoseInFrame contains a pose and the and the reference frame in which it was observed"""
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     REFERENCE_FRAME_FIELD_NUMBER: builtins.int
     POSE_FIELD_NUMBER: builtins.int
@@ -203,6 +220,7 @@ class RectangularPrism(google.protobuf.message.Message):
 global___RectangularPrism = RectangularPrism
 
 class Geometry(google.protobuf.message.Message):
+    """Geometry contains the dimensions of a given geometry and the pose of its center. The geometry is one of either a sphere or a box."""
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     CENTER_FIELD_NUMBER: builtins.int
     SPHERE_FIELD_NUMBER: builtins.int
@@ -210,7 +228,8 @@ class Geometry(google.protobuf.message.Message):
 
     @property
     def center(self) -> global___Pose:
-        ...
+        """Pose of a gemetries center point"""
+        pass
 
     @property
     def sphere(self) -> global___Sphere:
@@ -234,14 +253,19 @@ class Geometry(google.protobuf.message.Message):
 global___Geometry = Geometry
 
 class GeometriesInFrame(google.protobuf.message.Message):
+    """GeometriesinFrame contains the dimensions of a given geometry, pose of its center point, and the reference frame by which it was 
+    observed.
+    """
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     REFERENCE_FRAME_FIELD_NUMBER: builtins.int
     GEOMETRIES_FIELD_NUMBER: builtins.int
     reference_frame: typing.Text = ...
+    'Reference frame of the observer of the geometry'
 
     @property
     def geometries(self) -> google.protobuf.internal.containers.RepeatedCompositeFieldContainer[global___Geometry]:
-        ...
+        """Dimensional type"""
+        pass
 
     def __init__(self, *, reference_frame: typing.Text=..., geometries: typing.Optional[typing.Iterable[global___Geometry]]=...) -> None:
         ...
@@ -251,6 +275,9 @@ class GeometriesInFrame(google.protobuf.message.Message):
 global___GeometriesInFrame = GeometriesInFrame
 
 class PointCloudObject(google.protobuf.message.Message):
+    """PointCloudObject contains an image in bytes with point cloud data of all of the objects captured by a given observer as well as a 
+    repeated list of geometries which respresents the center point and geometry of each of the objects within the point cloud
+    """
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     POINT_CLOUD_FIELD_NUMBER: builtins.int
     GEOMETRIES_FIELD_NUMBER: builtins.int
@@ -287,6 +314,10 @@ class GeoPoint(google.protobuf.message.Message):
 global___GeoPoint = GeoPoint
 
 class Transform(google.protobuf.message.Message):
+    """Transform contains a pose and two reference frames. The first reference frame is the starting reference frame, and the second reference
+    frame is the observer reference frame. The second reference frame has a pose which represents the pose of an object in the first
+    reference frame as observed within the second reference frame.
+    """
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     REFERENCE_FRAME_FIELD_NUMBER: builtins.int
     POSE_IN_OBSERVER_FRAME_FIELD_NUMBER: builtins.int
@@ -295,9 +326,7 @@ class Transform(google.protobuf.message.Message):
 
     @property
     def pose_in_observer_frame(self) -> global___PoseInFrame:
-        """the pose of the above reference frame with respect
-        to a different observer reference frame
-        """
+        """the pose of the above reference frame with respect to a different observer reference frame"""
         pass
 
     def __init__(self, *, reference_frame: typing.Text=..., pose_in_observer_frame: typing.Optional[global___PoseInFrame]=...) -> None:
@@ -311,19 +340,22 @@ class Transform(google.protobuf.message.Message):
 global___Transform = Transform
 
 class WorldState(google.protobuf.message.Message):
+    """WorldState contains information about the physical environment around a given robot. All of the fields within this message are optional, 
+    they can include information about the physical dimensions of an obstacle, the freespace of a robot, and any desired transforms between a
+    given reference frame and a new target reference frame.
+    """
     DESCRIPTOR: google.protobuf.descriptor.Descriptor = ...
     OBSTACLES_FIELD_NUMBER: builtins.int
     TRANSFORMS_FIELD_NUMBER: builtins.int
 
     @property
     def obstacles(self) -> google.protobuf.internal.containers.RepeatedCompositeFieldContainer[global___GeometriesInFrame]:
-        ...
+        """a list of obstacles expressed as a geomtry and the reference frame in which it was observed; this field is optional"""
+        pass
 
     @property
     def transforms(self) -> google.protobuf.internal.containers.RepeatedCompositeFieldContainer[global___Transform]:
-        """a list of Transforms needed to transform a pose
-        from one reference frame to another; this field is optional
-        """
+        """a list of Transforms needed to transform a pose from one reference frame to another; this field is optional"""
         pass
 
     def __init__(self, *, obstacles: typing.Optional[typing.Iterable[global___GeometriesInFrame]]=..., transforms: typing.Optional[typing.Iterable[global___Transform]]=...) -> None:
