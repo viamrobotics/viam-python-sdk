df7d6d7472a3682fd7560bbd4c718a9879ae331c
Fahmina <fahmina.ayesha.ahmed@gmail.com>
2022-04-04 17:36:48 -0400

    documentation for services (#67)
    * documentation for services (frame system, motion, object_segmention) 
    
    Co-authored-by: Fahmina Ahmed <fahmina@Fahminas-MacBook-Air.local>

--- viam/services/frame_system.py
+++ viam/services/frame_system.py
@@ -9,11 +9,21 @@ from viam.proto.api.service.framesystem import (Config, ConfigRequest,
 
 
 class FrameSystemClient:
+    """
+    FrameSystem is a Viam service that handles tracking and transforming the reference frames of observations made by different components
+    of a robot.
+
+    """
 
     def __init__(self, channel: Channel) -> None:
         self.client = FrameSystemServiceStub(channel)
 
     async def config(self) -> List[Config]:
+        """
+        Get the configuration of the frame system of a given robot.
+
+        Returns (Config): The configuration of a given robot's frame system.
+        """
         request = ConfigRequest()
         response: ConfigResponse = await self.client.Config(request)
         return list(response.frame_system_configs)
@@ -23,6 +33,15 @@ class FrameSystemClient:
         source: PoseInFrame,
         destination: str
     ) -> PoseInFrame:
+        """
+        Transform a given source Pose from the reference frame to a new specified destination which is a reference frame.
+
+        Args:
+
+            source (Pose): The pose that needs to be transformed.
+            destination (str) : The name of the reference frame to transform the given pose to.
+
+        """
         request = TransformPoseRequest(source=source, destination=destination)
         response: TransformPoseResponse = \
             await self.client.TransformPose(request)
--- viam/services/motion.py
+++ viam/services/motion.py
@@ -8,6 +8,11 @@ from viam.proto.api.service.motion import (GetPoseRequest, GetPoseResponse,
 
 
 class MotionClient:
+    """ Motion is a Viam service that coordinates motion planning across all of the components in a given robot.
+
+    The motion planning service calculates a valid path that avoids self collision by default. If additional constraints are supplied in the
+    `world_state` message, the motion planning service will also account for those.
+     """
 
     def __init__(self, channel: Channel):
         self.client = MotionServiceStub(channel)
@@ -18,6 +23,15 @@ class MotionClient:
         destination: PoseInFrame,
         world_state: Optional[WorldState] = None
     ) -> bool:
+        """
+        Move a component on the robot to a given destination, obey any constraints (transforms or obstacles) if supplied in WorldState.
+
+        Args:
+            resource_name (ResourceName): Name of a component on a given robot.
+            destination (PoseInFrame): The destination to move to, expressed as a `Pose` and the frame in which it was observed.
+            world_state (WorldState): When supplied, the motion service will create a plan that obeys any contraints expressed in the
+                WorldState message.
+        """
         request = MoveRequest(
             destination=destination,
             component_name=resource_name,
@@ -31,6 +45,16 @@ class MotionClient:
         resource_name: ResourceName,
         destination_frame: str
     ) -> PoseInFrame:
+        """
+        Get the Pose and observer frame for any given component on a robot.
+
+        Args:
+            component_name(ResourceName): Name of a component on a robot.
+            destination_frame(str):
+
+        Returns:
+            Pose(PoseInFrame): Pose of the given component and the frame in which it was observed.
+        """
         request = GetPoseRequest(
             component_name=resource_name,
             destination_frame=destination_frame
