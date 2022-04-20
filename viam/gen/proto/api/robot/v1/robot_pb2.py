fc024948250119e17c4b625297108feaf5d9fc11
viambot <viambot@users.noreply.github.com>
2022-04-20 13:50:14 +0000

    [WORKFLOW] Updating protos from viamrobotics/rdk, commit: bf6380b79275b60a33331daad505b892e34c0997

--- viam/gen/proto/api/robot/v1/robot_pb2.py
+++ viam/gen/proto/api/robot/v1/robot_pb2.py
@@ -1,8 +1,7 @@
 """Generated protocol buffer code."""
+from google.protobuf.internal import builder as _builder
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
@@ -10,40 +9,8 @@ from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from .....proto.api.common.v1 import common_pb2 as proto_dot_api_dot_common_dot_v1_dot_common__pb2
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1eproto/api/robot/v1/robot.proto\x12\x12proto.api.robot.v1\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1cgoogle/api/annotations.proto\x1a proto/api/common/v1/common.proto"%\n\x0fDoActionRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name"\x12\n\x10DoActionResponse"\x90\x01\n\x19ResourceRunCommandRequest\x12#\n\rresource_name\x18\x01 \x01(\tR\x0cresourceName\x12!\n\x0ccommand_name\x18\x02 \x01(\tR\x0bcommandName\x12+\n\x04args\x18\x03 \x01(\x0b2\x17.google.protobuf.StructR\x04args"M\n\x1aResourceRunCommandResponse\x12/\n\x06result\x18\x01 \x01(\x0b2\x17.google.protobuf.StructR\x06result"\xa0\x01\n\tOperation\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id\x12\x16\n\x06method\x18\x02 \x01(\tR\x06method\x125\n\targuments\x18\x03 \x01(\x0b2\x17.google.protobuf.StructR\targuments\x124\n\x07started\x18\x04 \x01(\x0b2\x1a.google.protobuf.TimestampR\x07started"\x16\n\x14GetOperationsRequest"V\n\x15GetOperationsResponse\x12=\n\noperations\x18\x01 \x03(\x0b2\x1d.proto.api.robot.v1.OperationR\noperations"(\n\x16CancelOperationRequest\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id"\x19\n\x17CancelOperationResponse"*\n\x18BlockForOperationRequest\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id"\x1b\n\x19BlockForOperationResponse2\xfb\x04\n\x0cRobotService\x12\xae\x01\n\x12ResourceRunCommand\x12-.proto.api.robot.v1.ResourceRunCommandRequest\x1a..proto.api.robot.v1.ResourceRunCommandResponse"9\x82\xd3\xe4\x93\x023"1/viam/api/v1/resource/{resource_name}/run_command\x12\x8a\x01\n\rGetOperations\x12(.proto.api.robot.v1.GetOperationsRequest\x1a).proto.api.robot.v1.GetOperationsResponse"$\x82\xd3\xe4\x93\x02\x1e\x12\x1c/viam/api/v1/operations/list\x12\x92\x01\n\x0fCancelOperation\x12*.proto.api.robot.v1.CancelOperationRequest\x1a+.proto.api.robot.v1.CancelOperationResponse"&\x82\xd3\xe4\x93\x02 "\x1e/viam/api/v1/operations/cancel\x12\x97\x01\n\x11BlockForOperation\x12,.proto.api.robot.v1.BlockForOperationRequest\x1a-.proto.api.robot.v1.BlockForOperationResponse"%\x82\xd3\xe4\x93\x02\x1f"\x1d/viam/api/v1/operations/blockBE\n\x1fcom.viam.rdk.proto.api.robot.v1Z"go.viam.com/rdk/proto/api/robot/v1b\x06proto3')
-_DOACTIONREQUEST = DESCRIPTOR.message_types_by_name['DoActionRequest']
-_DOACTIONRESPONSE = DESCRIPTOR.message_types_by_name['DoActionResponse']
-_RESOURCERUNCOMMANDREQUEST = DESCRIPTOR.message_types_by_name['ResourceRunCommandRequest']
-_RESOURCERUNCOMMANDRESPONSE = DESCRIPTOR.message_types_by_name['ResourceRunCommandResponse']
-_OPERATION = DESCRIPTOR.message_types_by_name['Operation']
-_GETOPERATIONSREQUEST = DESCRIPTOR.message_types_by_name['GetOperationsRequest']
-_GETOPERATIONSRESPONSE = DESCRIPTOR.message_types_by_name['GetOperationsResponse']
-_CANCELOPERATIONREQUEST = DESCRIPTOR.message_types_by_name['CancelOperationRequest']
-_CANCELOPERATIONRESPONSE = DESCRIPTOR.message_types_by_name['CancelOperationResponse']
-_BLOCKFOROPERATIONREQUEST = DESCRIPTOR.message_types_by_name['BlockForOperationRequest']
-_BLOCKFOROPERATIONRESPONSE = DESCRIPTOR.message_types_by_name['BlockForOperationResponse']
-DoActionRequest = _reflection.GeneratedProtocolMessageType('DoActionRequest', (_message.Message,), {'DESCRIPTOR': _DOACTIONREQUEST, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(DoActionRequest)
-DoActionResponse = _reflection.GeneratedProtocolMessageType('DoActionResponse', (_message.Message,), {'DESCRIPTOR': _DOACTIONRESPONSE, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(DoActionResponse)
-ResourceRunCommandRequest = _reflection.GeneratedProtocolMessageType('ResourceRunCommandRequest', (_message.Message,), {'DESCRIPTOR': _RESOURCERUNCOMMANDREQUEST, '__module__': 'proto.api.robot.v1.robot_pb2', '__doc__': 'Attributes:\n      resource_name:\n          Note(erd): okay in v1 because names are unique. v2 should be a\n          VRN\n  '})
-_sym_db.RegisterMessage(ResourceRunCommandRequest)
-ResourceRunCommandResponse = _reflection.GeneratedProtocolMessageType('ResourceRunCommandResponse', (_message.Message,), {'DESCRIPTOR': _RESOURCERUNCOMMANDRESPONSE, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(ResourceRunCommandResponse)
-Operation = _reflection.GeneratedProtocolMessageType('Operation', (_message.Message,), {'DESCRIPTOR': _OPERATION, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(Operation)
-GetOperationsRequest = _reflection.GeneratedProtocolMessageType('GetOperationsRequest', (_message.Message,), {'DESCRIPTOR': _GETOPERATIONSREQUEST, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(GetOperationsRequest)
-GetOperationsResponse = _reflection.GeneratedProtocolMessageType('GetOperationsResponse', (_message.Message,), {'DESCRIPTOR': _GETOPERATIONSRESPONSE, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(GetOperationsResponse)
-CancelOperationRequest = _reflection.GeneratedProtocolMessageType('CancelOperationRequest', (_message.Message,), {'DESCRIPTOR': _CANCELOPERATIONREQUEST, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(CancelOperationRequest)
-CancelOperationResponse = _reflection.GeneratedProtocolMessageType('CancelOperationResponse', (_message.Message,), {'DESCRIPTOR': _CANCELOPERATIONRESPONSE, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(CancelOperationResponse)
-BlockForOperationRequest = _reflection.GeneratedProtocolMessageType('BlockForOperationRequest', (_message.Message,), {'DESCRIPTOR': _BLOCKFOROPERATIONREQUEST, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(BlockForOperationRequest)
-BlockForOperationResponse = _reflection.GeneratedProtocolMessageType('BlockForOperationResponse', (_message.Message,), {'DESCRIPTOR': _BLOCKFOROPERATIONRESPONSE, '__module__': 'proto.api.robot.v1.robot_pb2'})
-_sym_db.RegisterMessage(BlockForOperationResponse)
-_ROBOTSERVICE = DESCRIPTOR.services_by_name['RobotService']
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.api.robot.v1.robot_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
     DESCRIPTOR._serialized_options = b'\n\x1fcom.viam.rdk.proto.api.robot.v1Z"go.viam.com/rdk/proto/api/robot/v1'
--- viam/gen/proto/api/service/status/v1/status_pb2.py
+++ viam/gen/proto/api/service/status/v1/status_pb2.py
@@ -1,8 +1,7 @@
 """Generated protocol buffer code."""
+from google.protobuf.internal import builder as _builder
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
@@ -10,22 +9,8 @@ from google.protobuf import duration_pb2 as google_dot_protobuf_dot_duration__pb
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from ......proto.api.common.v1 import common_pb2 as proto_dot_api_dot_common_dot_v1_dot_common__pb2
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n(proto/api/service/status/v1/status.proto\x12\x1bproto.api.service.status.v1\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1egoogle/protobuf/duration.proto\x1a\x1cgoogle/api/annotations.proto\x1a proto/api/common/v1/common.proto"p\n\x06Status\x125\n\x04name\x18\x01 \x01(\x0b2!.proto.api.common.v1.ResourceNameR\x04name\x12/\n\x06status\x18\x02 \x01(\x0b2\x17.google.protobuf.StructR\x06status"\\\n\x10GetStatusRequest\x12H\n\x0eresource_names\x18\x01 \x03(\x0b2!.proto.api.common.v1.ResourceNameR\rresourceNames"P\n\x11GetStatusResponse\x12;\n\x06status\x18\x01 \x03(\x0b2#.proto.api.service.status.v1.StatusR\x06status"\x90\x01\n\x13StreamStatusRequest\x12H\n\x0eresource_names\x18\x01 \x03(\x0b2!.proto.api.common.v1.ResourceNameR\rresourceNames\x12/\n\x05every\x18\x02 \x01(\x0b2\x19.google.protobuf.DurationR\x05every"S\n\x14StreamStatusResponse\x12;\n\x06status\x18\x01 \x03(\x0b2#.proto.api.service.status.v1.StatusR\x06status2\xc5\x02\n\rStatusService\x12\x8f\x01\n\tGetStatus\x12-.proto.api.service.status.v1.GetStatusRequest\x1a..proto.api.service.status.v1.GetStatusResponse"#\x82\xd3\xe4\x93\x02\x1d\x12\x1b/viam/api/v1/service/status\x12\xa1\x01\n\x0cStreamStatus\x120.proto.api.service.status.v1.StreamStatusRequest\x1a1.proto.api.service.status.v1.StreamStatusResponse"*\x82\xd3\xe4\x93\x02$\x12"/viam/api/v1/service/status/stream0\x01BW\n(com.viam.rdk.proto.api.service.status.v1Z+go.viam.com/rdk/proto/api/service/status/v1b\x06proto3')
-_STATUS = DESCRIPTOR.message_types_by_name['Status']
-_GETSTATUSREQUEST = DESCRIPTOR.message_types_by_name['GetStatusRequest']
-_GETSTATUSRESPONSE = DESCRIPTOR.message_types_by_name['GetStatusResponse']
-_STREAMSTATUSREQUEST = DESCRIPTOR.message_types_by_name['StreamStatusRequest']
-_STREAMSTATUSRESPONSE = DESCRIPTOR.message_types_by_name['StreamStatusResponse']
-Status = _reflection.GeneratedProtocolMessageType('Status', (_message.Message,), {'DESCRIPTOR': _STATUS, '__module__': 'proto.api.service.status.v1.status_pb2'})
-_sym_db.RegisterMessage(Status)
-GetStatusRequest = _reflection.GeneratedProtocolMessageType('GetStatusRequest', (_message.Message,), {'DESCRIPTOR': _GETSTATUSREQUEST, '__module__': 'proto.api.service.status.v1.status_pb2'})
-_sym_db.RegisterMessage(GetStatusRequest)
-GetStatusResponse = _reflection.GeneratedProtocolMessageType('GetStatusResponse', (_message.Message,), {'DESCRIPTOR': _GETSTATUSRESPONSE, '__module__': 'proto.api.service.status.v1.status_pb2'})
-_sym_db.RegisterMessage(GetStatusResponse)
-StreamStatusRequest = _reflection.GeneratedProtocolMessageType('StreamStatusRequest', (_message.Message,), {'DESCRIPTOR': _STREAMSTATUSREQUEST, '__module__': 'proto.api.service.status.v1.status_pb2', '__doc__': 'Attributes:\n      every:\n          how often to send a new status.\n  '})
-_sym_db.RegisterMessage(StreamStatusRequest)
-StreamStatusResponse = _reflection.GeneratedProtocolMessageType('StreamStatusResponse', (_message.Message,), {'DESCRIPTOR': _STREAMSTATUSRESPONSE, '__module__': 'proto.api.service.status.v1.status_pb2'})
-_sym_db.RegisterMessage(StreamStatusResponse)
-_STATUSSERVICE = DESCRIPTOR.services_by_name['StatusService']
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.api.service.status.v1.status_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
     DESCRIPTOR._serialized_options = b'\n(com.viam.rdk.proto.api.service.status.v1Z+go.viam.com/rdk/proto/api/service/status/v1'
