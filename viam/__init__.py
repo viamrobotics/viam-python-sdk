a6653cd4703cc6fa114405b070757fa1c3d6b56c
Naveed Jooma <naveed@viam.com>
2022-05-16 19:05:34 -0400

    Fixing double log messages

--- viam/__init__.py
+++ viam/__init__.py
@@ -12,12 +12,10 @@ __docformat__ = "restructuredtext"
 _TASK_PREFIX = uuid4().hex
 
 
-LOGGER = getLogger(__name__)
-
-
-def my_except_hook(exctype, value, traceback):
-    LOGGER.error('[ERROR] Uncaught exception', exc_info=(exctype, value, traceback))
+def _log_exceptions(exctype, value, traceback):
+    _LOGGER = getLogger(__name__)
+    _LOGGER.error('[ERROR] Uncaught exception', exc_info=(exctype, value, traceback))
     sys.__excepthook__(exctype, value, traceback)
 
 
-sys.excepthook = my_except_hook
+sys.excepthook = _log_exceptions
