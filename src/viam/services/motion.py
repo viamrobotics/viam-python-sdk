cbe4ea6fa8dd5441dd7467c8577b2f57ec0ab403
Naveed Jooma <naveed@viam.com>
2022-08-10 12:16:00 -0400

    Update motion service

--- src/viam/services/motion.py
+++ src/viam/services/motion.py
@@ -2,7 +2,15 @@ from typing import Optional
 
 from grpclib.client import Channel
 from viam.proto.api.common import PoseInFrame, ResourceName, WorldState
-from viam.proto.api.service.motion import GetPoseRequest, GetPoseResponse, MotionServiceStub, MoveRequest, MoveResponse
+from viam.proto.api.service.motion import (
+    GetPoseRequest,
+    GetPoseResponse,
+    MotionServiceStub,
+    MoveSingleComponentRequest,
+    MoveSingleComponentResponse,
+    PlanAndMoveRequest,
+    PlanAndMoveResponse,
+)
 
 
 class MotionClient:
@@ -15,18 +23,43 @@ class MotionClient:
     def __init__(self, channel: Channel):
         self.client = MotionServiceStub(channel)
 
-    async def move(self, component_name: ResourceName, destination: PoseInFrame, world_state: Optional[WorldState] = None) -> bool:
-        """
-        Move a component on the robot to a given destination, obey any constraints (transforms or obstacles) if supplied in WorldState.
+    async def plan_and_move(self, component_name: ResourceName, destination: PoseInFrame, world_state: Optional[WorldState] = None) -> bool:
+        """Plan and execute a movement to move the component specified to its goal destination.
+
 
         Args:
             component_name (ResourceName): Name of a component on a given robot.
             destination (PoseInFrame): The destination to move to, expressed as a `Pose` and the frame in which it was observed.
             world_state (WorldState): When supplied, the motion service will create a plan that obeys any contraints expressed in the
                 WorldState message.
+
+        Returns:
+            bool: Whether the move was successful
         """
-        request = MoveRequest(destination=destination, component_name=component_name, world_state=world_state)
-        response: MoveResponse = await self.client.Move(request)
+        request = PlanAndMoveRequest(destination=destination, component_name=component_name, world_state=world_state)
+        response: PlanAndMoveResponse = await self.client.PlanAndMove(request)
+        return response.success
+
+    async def move_single_component(
+        self, component_name: ResourceName, destination: PoseInFrame, world_state: Optional[WorldState] = None
+    ) -> bool:
+        """
+        This function will pass through a move command to a component with a `move_to_position` method that takes a `Pose`. `Arm`s are the
+        only component that support this. This method will transform the destination pose, given in an arbitrary frame, into the pose of the
+        arm. The arm will then move its most distal link to that pose. If you instead wish to move any other component than the arm end to
+        that pose, then you must manually adjust the given destination by the transform from the arm end to the intended component.
+
+        Args:
+            component_name (ResourceName): Name of a component on a given robot.
+            destination (PoseInFrame): The destination to move to, expressed as a `Pose` and the frame in which it was observed.
+            world_state (WorldState): When supplied, the motion service will create a plan that obeys any contraints expressed in the
+                WorldState message.
+
+        Returns:
+            bool: Whether the move was successful
+        """
+        request = MoveSingleComponentRequest(destination=destination, component_name=component_name, world_state=world_state)
+        response: MoveSingleComponentResponse = await self.client.MoveSingleComponent(request)
         return response.success
 
     async def get_pose(self, component_name: ResourceName, destination_frame: str) -> PoseInFrame:
--- tests/test_motion_service.py
+++ tests/test_motion_service.py
@@ -7,64 +7,47 @@ from viam.services.motion import MotionClient
 
 from .mocks.services import MockMotionService
 
-MOVE_RESPONSES = {
-    'arm': True,
-    'gantry': False
-}
+PLAN_AND_MOVE_RESPONSES = {"arm": False, "gantry": True}
+MOVE_SINGLE_COMPONENT_RESPONSES = {"arm": True, "gantry": False}
 GET_POSE_RESPONSES = {
-    'arm': PoseInFrame(
-        reference_frame='arm',
-        pose=Pose(
-            x=1,
-            y=2,
-            z=3,
-            o_x=2,
-            o_y=3,
-            o_z=4,
-            theta=20
-        )
-    ),
-    'gantry': PoseInFrame(
-        reference_frame='gantry',
-        pose=Pose(
-            x=2,
-            y=3,
-            z=4,
-            o_x=3,
-            o_y=4,
-            o_z=5,
-            theta=21
-        )
-    )
+    "arm": PoseInFrame(reference_frame="arm", pose=Pose(x=1, y=2, z=3, o_x=2, o_y=3, o_z=4, theta=20)),
+    "gantry": PoseInFrame(reference_frame="gantry", pose=Pose(x=2, y=3, z=4, o_x=3, o_y=4, o_z=5, theta=21)),
 }
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def service() -> MockMotionService:
     return MockMotionService(
-        move_responses=MOVE_RESPONSES,
-        get_pose_responses=GET_POSE_RESPONSES
+        plan_and_move_responses=PLAN_AND_MOVE_RESPONSES,
+        move_single_component_responses=MOVE_SINGLE_COMPONENT_RESPONSES,
+        get_pose_responses=GET_POSE_RESPONSES,
     )
 
 
 class TestClient:
     @pytest.mark.asyncio
-    async def test_move(self, service: MockMotionService):
+    async def test_plan_and_move(self, service: MockMotionService):
         async with ChannelFor([service]) as channel:
             client = MotionClient(channel)
-            success = await client.move(
-                Arm.get_resource_name('arm'), PoseInFrame())
-            assert success == MOVE_RESPONSES['arm']
-            success = await client.move(
-                Gantry.get_resource_name('gantry'), PoseInFrame())
-            assert success == MOVE_RESPONSES['gantry']
+            success = await client.plan_and_move(Arm.get_resource_name("arm"), PoseInFrame())
+            assert success == PLAN_AND_MOVE_RESPONSES["arm"]
+            success = await client.plan_and_move(Gantry.get_resource_name("gantry"), PoseInFrame())
+            assert success == PLAN_AND_MOVE_RESPONSES["gantry"]
+
+    @pytest.mark.asyncio
+    async def test_move_single_component(self, service: MockMotionService):
+        async with ChannelFor([service]) as channel:
+            client = MotionClient(channel)
+            success = await client.move_single_component(Arm.get_resource_name("arm"), PoseInFrame())
+            assert success == MOVE_SINGLE_COMPONENT_RESPONSES["arm"]
+            success = await client.move_single_component(Gantry.get_resource_name("gantry"), PoseInFrame())
+            assert success == MOVE_SINGLE_COMPONENT_RESPONSES["gantry"]
 
     @pytest.mark.asyncio
     async def test_get_pose(self, service: MockMotionService):
         async with ChannelFor([service]) as channel:
             client = MotionClient(channel)
-            pose = await client.get_pose(Arm.get_resource_name('arm'), 'x')
-            assert pose == GET_POSE_RESPONSES['arm']
-            pose = await client.get_pose(
-                Gantry.get_resource_name('gantry'), 'y')
-            assert pose == GET_POSE_RESPONSES['gantry']
+            pose = await client.get_pose(Arm.get_resource_name("arm"), "x")
+            assert pose == GET_POSE_RESPONSES["arm"]
+            pose = await client.get_pose(Gantry.get_resource_name("gantry"), "y")
+            assert pose == GET_POSE_RESPONSES["gantry"]
