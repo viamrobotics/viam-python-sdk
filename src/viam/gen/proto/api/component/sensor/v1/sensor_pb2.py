da56d2f6c80731f05293a9e146128e1ffa16b3b2
viambot <viambot@users.noreply.github.com>
2022-08-22 16:36:16 +0000

    [WORKFLOW] Updating protos from viamrobotics/rdk, commit: 2a3205959a68e8d0d3bebac4da55c92463270ccf

--- src/viam/gen/proto/api/component/sensor/v1/sensor_pb2.py
+++ src/viam/gen/proto/api/component/sensor/v1/sensor_pb2.py
@@ -6,17 +6,21 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*proto/api/component/sensor/v1/sensor.proto\x12\x1dproto.api.component.sensor.v1\x1a\x1cgoogle/api/annotations.proto\x1a\x1cgoogle/protobuf/struct.proto"(\n\x12GetReadingsRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name"I\n\x13GetReadingsResponse\x122\n\x08readings\x18\x01 \x03(\x0b2\x16.google.protobuf.ValueR\x08readings2\xbd\x01\n\rSensorService\x12\xab\x01\n\x0bGetReadings\x121.proto.api.component.sensor.v1.GetReadingsRequest\x1a2.proto.api.component.sensor.v1.GetReadingsResponse"5\x82\xd3\xe4\x93\x02/\x12-/viam/api/v1/component/sensor/{name}/readingsB[\n*com.viam.rdk.proto.api.component.sensor.v1Z-go.viam.com/rdk/proto/api/component/sensor/v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*proto/api/component/sensor/v1/sensor.proto\x12\x1dproto.api.component.sensor.v1\x1a\x1cgoogle/api/annotations.proto\x1a\x1cgoogle/protobuf/struct.proto"(\n\x12GetReadingsRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name"\xc8\x01\n\x13GetReadingsResponse\x12\\\n\x08readings\x18\x01 \x03(\x0b2@.proto.api.component.sensor.v1.GetReadingsResponse.ReadingsEntryR\x08readings\x1aS\n\rReadingsEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12,\n\x05value\x18\x02 \x01(\x0b2\x16.google.protobuf.ValueR\x05value:\x028\x012\xbd\x01\n\rSensorService\x12\xab\x01\n\x0bGetReadings\x121.proto.api.component.sensor.v1.GetReadingsRequest\x1a2.proto.api.component.sensor.v1.GetReadingsResponse"5\x82\xd3\xe4\x93\x02/\x12-/viam/api/v1/component/sensor/{name}/readingsB[\n*com.viam.rdk.proto.api.component.sensor.v1Z-go.viam.com/rdk/proto/api/component/sensor/v1b\x06proto3')
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.api.component.sensor.v1.sensor_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
     DESCRIPTOR._options = None
     DESCRIPTOR._serialized_options = b'\n*com.viam.rdk.proto.api.component.sensor.v1Z-go.viam.com/rdk/proto/api/component/sensor/v1'
+    _GETREADINGSRESPONSE_READINGSENTRY._options = None
+    _GETREADINGSRESPONSE_READINGSENTRY._serialized_options = b'8\x01'
     _SENSORSERVICE.methods_by_name['GetReadings']._options = None
     _SENSORSERVICE.methods_by_name['GetReadings']._serialized_options = b'\x82\xd3\xe4\x93\x02/\x12-/viam/api/v1/component/sensor/{name}/readings'
     _GETREADINGSREQUEST._serialized_start = 137
     _GETREADINGSREQUEST._serialized_end = 177
-    _GETREADINGSRESPONSE._serialized_start = 179
-    _GETREADINGSRESPONSE._serialized_end = 252
-    _SENSORSERVICE._serialized_start = 255
-    _SENSORSERVICE._serialized_end = 444
+    _GETREADINGSRESPONSE._serialized_start = 180
+    _GETREADINGSRESPONSE._serialized_end = 380
+    _GETREADINGSRESPONSE_READINGSENTRY._serialized_start = 297
+    _GETREADINGSRESPONSE_READINGSENTRY._serialized_end = 380
+    _SENSORSERVICE._serialized_start = 383
+    _SENSORSERVICE._serialized_end = 572
