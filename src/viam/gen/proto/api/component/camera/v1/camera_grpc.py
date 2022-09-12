7bb563ca1a4bf11ea232c72789021eada0b38d88
viambot <viambot@users.noreply.github.com>
2022-09-12 17:46:56 +0000

    [WORKFLOW] Updating protos from viamrobotics/rdk, commit: f04a4fa4864ec67318b57a1386b993dde62ae997

--- src/viam/gen/proto/api/component/camera/v1/camera_grpc.py
+++ src/viam/gen/proto/api/component/camera/v1/camera_grpc.py
@@ -11,7 +11,7 @@ from ...... import proto
 class CameraServiceBase(abc.ABC):
 
     @abc.abstractmethod
-    async def GetFrame(self, stream: 'grpclib.server.Stream[proto.api.component.camera.v1.camera_pb2.GetFrameRequest, proto.api.component.camera.v1.camera_pb2.GetFrameResponse]') -> None:
+    async def GetImage(self, stream: 'grpclib.server.Stream[proto.api.component.camera.v1.camera_pb2.GetImageRequest, proto.api.component.camera.v1.camera_pb2.GetImageResponse]') -> None:
         pass
 
     @abc.abstractmethod
@@ -27,12 +27,12 @@ class CameraServiceBase(abc.ABC):
         pass
 
     def __mapping__(self) -> typing.Dict[str, grpclib.const.Handler]:
-        return {'/proto.api.component.camera.v1.CameraService/GetFrame': grpclib.const.Handler(self.GetFrame, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.GetFrameRequest, proto.api.component.camera.v1.camera_pb2.GetFrameResponse), '/proto.api.component.camera.v1.CameraService/RenderFrame': grpclib.const.Handler(self.RenderFrame, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.RenderFrameRequest, google.api.httpbody_pb2.HttpBody), '/proto.api.component.camera.v1.CameraService/GetPointCloud': grpclib.const.Handler(self.GetPointCloud, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.GetPointCloudRequest, proto.api.component.camera.v1.camera_pb2.GetPointCloudResponse), '/proto.api.component.camera.v1.CameraService/GetProperties': grpclib.const.Handler(self.GetProperties, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.GetPropertiesRequest, proto.api.component.camera.v1.camera_pb2.GetPropertiesResponse)}
+        return {'/proto.api.component.camera.v1.CameraService/GetImage': grpclib.const.Handler(self.GetImage, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.GetImageRequest, proto.api.component.camera.v1.camera_pb2.GetImageResponse), '/proto.api.component.camera.v1.CameraService/RenderFrame': grpclib.const.Handler(self.RenderFrame, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.RenderFrameRequest, google.api.httpbody_pb2.HttpBody), '/proto.api.component.camera.v1.CameraService/GetPointCloud': grpclib.const.Handler(self.GetPointCloud, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.GetPointCloudRequest, proto.api.component.camera.v1.camera_pb2.GetPointCloudResponse), '/proto.api.component.camera.v1.CameraService/GetProperties': grpclib.const.Handler(self.GetProperties, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.camera.v1.camera_pb2.GetPropertiesRequest, proto.api.component.camera.v1.camera_pb2.GetPropertiesResponse)}
 
 class CameraServiceStub:
 
     def __init__(self, channel: grpclib.client.Channel) -> None:
-        self.GetFrame = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.camera.v1.CameraService/GetFrame', proto.api.component.camera.v1.camera_pb2.GetFrameRequest, proto.api.component.camera.v1.camera_pb2.GetFrameResponse)
+        self.GetImage = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.camera.v1.CameraService/GetImage', proto.api.component.camera.v1.camera_pb2.GetImageRequest, proto.api.component.camera.v1.camera_pb2.GetImageResponse)
         self.RenderFrame = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.camera.v1.CameraService/RenderFrame', proto.api.component.camera.v1.camera_pb2.RenderFrameRequest, google.api.httpbody_pb2.HttpBody)
         self.GetPointCloud = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.camera.v1.CameraService/GetPointCloud', proto.api.component.camera.v1.camera_pb2.GetPointCloudRequest, proto.api.component.camera.v1.camera_pb2.GetPointCloudResponse)
         self.GetProperties = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.camera.v1.CameraService/GetProperties', proto.api.component.camera.v1.camera_pb2.GetPropertiesRequest, proto.api.component.camera.v1.camera_pb2.GetPropertiesResponse)
--- src/viam/gen/proto/api/component/generic/v1/generic_grpc.py
+++ src/viam/gen/proto/api/component/generic/v1/generic_grpc.py
@@ -11,13 +11,13 @@ from ...... import proto
 class GenericServiceBase(abc.ABC):
 
     @abc.abstractmethod
-    async def Do(self, stream: 'grpclib.server.Stream[proto.api.component.generic.v1.generic_pb2.DoRequest, proto.api.component.generic.v1.generic_pb2.DoResponse]') -> None:
+    async def DoCommand(self, stream: 'grpclib.server.Stream[proto.api.component.generic.v1.generic_pb2.DoCommandRequest, proto.api.component.generic.v1.generic_pb2.DoCommandResponse]') -> None:
         pass
 
     def __mapping__(self) -> typing.Dict[str, grpclib.const.Handler]:
-        return {'/proto.api.component.generic.v1.GenericService/Do': grpclib.const.Handler(self.Do, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.generic.v1.generic_pb2.DoRequest, proto.api.component.generic.v1.generic_pb2.DoResponse)}
+        return {'/proto.api.component.generic.v1.GenericService/DoCommand': grpclib.const.Handler(self.DoCommand, grpclib.const.Cardinality.UNARY_UNARY, proto.api.component.generic.v1.generic_pb2.DoCommandRequest, proto.api.component.generic.v1.generic_pb2.DoCommandResponse)}
 
 class GenericServiceStub:
 
     def __init__(self, channel: grpclib.client.Channel) -> None:
-        self.Do = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.generic.v1.GenericService/Do', proto.api.component.generic.v1.generic_pb2.DoRequest, proto.api.component.generic.v1.generic_pb2.DoResponse)
+        self.DoCommand = grpclib.client.UnaryUnaryMethod(channel, '/proto.api.component.generic.v1.GenericService/DoCommand', proto.api.component.generic.v1.generic_pb2.DoCommandRequest, proto.api.component.generic.v1.generic_pb2.DoCommandResponse)
