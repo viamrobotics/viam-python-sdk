8e6d681b53868505e5b86af7ac7287e5d47ea4b0
Naveed Jooma <naveed@viam.com>
2022-09-27 15:24:00 -0400

    Fix post install

--- etc/postinstall.sh
+++ etc/postinstall.sh
@@ -1,6 +1,6 @@
 rm -rf src/viam/rpc/libviam.* || true
 if [ "$(uname)" == "Linux" ]; then
-    curl -s -o src/viam/rpc/libviam.so https://github.com/viamrobotics/viam-rust-sdk/releases/latest/download/libviam-linux_$(uname -m).so
+    curl -sL -o src/viam/rpc/libviam.so https://github.com/viamrobotics/viam-rust-sdk/releases/latest/download/libviam-linux_$(uname -m).so
 elif [ "$(uname)" == "Darwin" ]; then
-    curl -s -o src/viam/rpc/libviam.dylib https://github.com/viamrobotics/viam-rust-sdk/releases/latest/download/libviam-macosx_$(uname -m).so
+    curl -sL -o src/viam/rpc/libviam.dylib https://github.com/viamrobotics/viam-rust-sdk/releases/latest/download/libviam-macosx_$(uname -m).dylib
 fi
