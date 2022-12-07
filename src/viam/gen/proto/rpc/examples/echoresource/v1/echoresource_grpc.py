cd7d4569c97c02fd5fc3b6581fbfaae4482b0382
viambot <viambot@users.noreply.github.com>
2022-12-07 18:07:14 +0000

    [WORKFLOW] Updating protos from viamrobotics/goutils, commit: ddce1e36d1b3568a9b9ede57c6acc8882f1c23e9

---
+++ src/viam/gen/proto/rpc/examples/echoresource/v1/echoresource_grpc.py
@@ -0,0 +1,31 @@
+import abc
+import typing
+import grpclib.const
+import grpclib.client
+if typing.TYPE_CHECKING:
+    import grpclib.server
+from ...... import proto
+
+class EchoResourceServiceBase(abc.ABC):
+
+    @abc.abstractmethod
+    async def EchoResource(self, stream: 'grpclib.server.Stream[proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceResponse]') -> None:
+        pass
+
+    @abc.abstractmethod
+    async def EchoResourceMultiple(self, stream: 'grpclib.server.Stream[proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceMultipleRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceMultipleResponse]') -> None:
+        pass
+
+    @abc.abstractmethod
+    async def EchoResourceBiDi(self, stream: 'grpclib.server.Stream[proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceBiDiRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceBiDiResponse]') -> None:
+        pass
+
+    def __mapping__(self) -> typing.Dict[str, grpclib.const.Handler]:
+        return {'/proto.rpc.examples.echoresource.v1.EchoResourceService/EchoResource': grpclib.const.Handler(self.EchoResource, grpclib.const.Cardinality.UNARY_UNARY, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceResponse), '/proto.rpc.examples.echoresource.v1.EchoResourceService/EchoResourceMultiple': grpclib.const.Handler(self.EchoResourceMultiple, grpclib.const.Cardinality.UNARY_STREAM, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceMultipleRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceMultipleResponse), '/proto.rpc.examples.echoresource.v1.EchoResourceService/EchoResourceBiDi': grpclib.const.Handler(self.EchoResourceBiDi, grpclib.const.Cardinality.STREAM_STREAM, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceBiDiRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceBiDiResponse)}
+
+class EchoResourceServiceStub:
+
+    def __init__(self, channel: grpclib.client.Channel) -> None:
+        self.EchoResource = grpclib.client.UnaryUnaryMethod(channel, '/proto.rpc.examples.echoresource.v1.EchoResourceService/EchoResource', proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceResponse)
+        self.EchoResourceMultiple = grpclib.client.UnaryStreamMethod(channel, '/proto.rpc.examples.echoresource.v1.EchoResourceService/EchoResourceMultiple', proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceMultipleRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceMultipleResponse)
+        self.EchoResourceBiDi = grpclib.client.StreamStreamMethod(channel, '/proto.rpc.examples.echoresource.v1.EchoResourceService/EchoResourceBiDi', proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceBiDiRequest, proto.rpc.examples.echoresource.v1.echoresource_pb2.EchoResourceBiDiResponse)
