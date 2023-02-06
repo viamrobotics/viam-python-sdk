8f46b9a58579cdf45d982a877a3cee4eb7960ec5
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-02-06 12:23:47 -0500

    Add tests

--- tests/test_resource_manager.py
+++ tests/test_resource_manager.py
@@ -36,6 +36,32 @@ class TestRegistration:
         with pytest.raises(DuplicateComponentError):
             manager.register(MockArm(name="servo2"))
 
+    def test_registers_correctly_with_remote_duplicate_name(self):
+        servo1 = MockServo(name="servo1")
+        servo3 = MockServo(name="servo3")
+        remote1_servo1 = MockServo(name="remote1:servo1")
+        remote1_servo2 = MockServo(name="remote1:servo2")
+        remote2_servo2 = MockServo(name="remote2:servo2")
+        remote2_servo3 = MockServo(name="remote2:servo3")
+        
+        manager = ResourceManager([])
+        manager.register(servo1)
+        manager.register(remote1_servo1)
+        assert "remote1:servo1" in manager.components
+        manager.register(remote1_servo2)
+        assert "servo2" in manager.components
+        assert "remote1:servo2" not in manager.components
+        manager.register(remote2_servo2)
+        assert "servo2" not in manager.components
+        assert "remote1:servo2" in manager.components
+        manager.register(remote2_servo3)
+        assert "servo3" in manager.components
+        tempservo = manager.components["servo3"]
+        manager.register(servo3)
+        assert "servo3" in manager.components
+        assert manager.components["remote2:servo3"] == tempservo
+        assert manager.components["servo3"] != tempservo
+
 
 class TestGetComponent:
     def test_get_component(self):
