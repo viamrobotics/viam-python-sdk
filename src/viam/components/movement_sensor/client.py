f0d2e0497ad8f92d54e392de9d8d55a0d6d942d1
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-04-21 18:18:08 -0400

    fix circular import

--- src/viam/components/movement_sensor/client.py
+++ src/viam/components/movement_sensor/client.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, Mapping, Optional, Tuple
 
 from grpclib.client import Channel
 
+from viam.components.movement_sensor.movement_sensor import MovementSensor
 from viam.proto.common import DoCommandRequest, DoCommandResponse
 from viam.proto.component.movementsensor import (
     GetAccuracyRequest,
@@ -25,7 +26,7 @@ from viam.proto.component.movementsensor import (
 from viam.resource.rpc_client_base import ReconfigurableResourceRPCClientBase
 from viam.utils import ValueTypes, dict_to_struct, struct_to_dict
 
-from . import GeoPoint, Orientation, MovementSensor, Vector3
+from . import GeoPoint, Orientation, Vector3
 
 
 class MovementSensorClient(MovementSensor, ReconfigurableResourceRPCClientBase):
