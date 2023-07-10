68d842d02fb4ea4648c91d362cefddae350f4fa5
Bashar Eid <bashar.eid@viam.com>
2023-07-10 17:27:05 -0400

    Implement mock server.

---
+++ docs/examples/_data_server.py
@@ -0,0 +1,117 @@
+import asyncio
+
+from grpclib.utils import graceful_exit
+from grpclib.server import Server, Stream
+
+from viam.proto.app.data import (
+    AddTagsToBinaryDataByFilterRequest,
+    AddTagsToBinaryDataByFilterResponse,
+    AddTagsToBinaryDataByIDsRequest,
+    AddTagsToBinaryDataByIDsResponse,
+    BinaryDataByFilterRequest,
+    BinaryDataByFilterResponse,
+    BinaryDataByIDsRequest,
+    BinaryDataByIDsResponse,
+    BoundingBoxLabelsByFilterRequest,
+    BoundingBoxLabelsByFilterResponse,
+    DataServiceBase,
+    DeleteBinaryDataByFilterRequest,
+    DeleteBinaryDataByFilterResponse,
+    DeleteBinaryDataByIDsRequest,
+    DeleteBinaryDataByIDsResponse,
+    DeleteTabularDataByFilterRequest,
+    DeleteTabularDataByFilterResponse,
+    RemoveTagsFromBinaryDataByFilterRequest,
+    RemoveTagsFromBinaryDataByFilterResponse,
+    RemoveTagsFromBinaryDataByIDsRequest,
+    RemoveTagsFromBinaryDataByIDsResponse,
+    TabularDataByFilterRequest,
+    TabularDataByFilterResponse,
+    TagsByFilterRequest,
+    TagsByFilterResponse,
+)
+from viam.proto.app.data_sync import (
+    AddBoundingBoxToImageByIDRequest,
+    AddBoundingBoxToImageByIDResponse,
+    DataCaptureUploadRequest,
+    DataCaptureUploadResponse,
+    DataSyncServiceBase,
+    FileUploadRequest,
+    FileUploadResponse,
+    RemoveBoundingBoxFromImageByIDRequest,
+    RemoveBoundingBoxFromImageByIDResponse
+)
+
+
+class DataServer(DataServiceBase, DataSyncServiceBase):
+
+    async def TabularDataByFilter(self, stream: [TabularDataByFilterRequest, TabularDataByFilterResponse]) -> None:
+        data = [{"PowerPct": 0, "IsPowerred": False}, {"PowerPct": 0, "IsPowered": False}, {"Position": 0}]
+        return data
+
+    async def BinaryDataByFilter(self, stream: Stream[BinaryDataByFilterRequest, BinaryDataByFilterResponse]) -> None:
+        pass
+
+    async def BinaryDataByIDs(self, stream: Stream[BinaryDataByIDsRequest, BinaryDataByIDsResponse]) -> None:
+        pass
+
+    async def DeleteTabularDataByFilter(self, stream: Stream[DeleteTabularDataByFilterRequest, DeleteTabularDataByFilterResponse]) -> None:
+        pass
+
+    async def DeleteBinaryDataByFilter(self, stream: Stream[DeleteBinaryDataByFilterRequest, DeleteBinaryDataByFilterResponse]) -> None:
+        pass
+
+    async def DeleteBinaryDataByIDs(self, stream: Stream[DeleteBinaryDataByIDsRequest, DeleteBinaryDataByIDsResponse]) -> None:
+        pass
+
+    async def AddTagsToBinaryDataByIDs(self, stream: Stream[AddTagsToBinaryDataByIDsRequest, AddTagsToBinaryDataByIDsResponse]) -> None:
+        pass
+
+    async def AddTagsToBinaryDataByFilter(
+        self,
+        stream: Stream[AddTagsToBinaryDataByFilterRequest, AddTagsToBinaryDataByFilterResponse]
+    ) -> None:
+        pass
+
+    async def RemoveTagsFromBinaryDataByIDs(
+        self,
+        stream: Stream[RemoveTagsFromBinaryDataByIDsRequest, RemoveTagsFromBinaryDataByIDsResponse]
+    ) -> None:
+        pass
+
+    async def RemoveTagsFromBinaryDataByFilter(
+        self,
+        stream: Stream[RemoveTagsFromBinaryDataByFilterRequest, RemoveTagsFromBinaryDataByFilterResponse]
+    ) -> None:
+        pass
+
+    async def TagsByFilter(self, stream: Stream[TagsByFilterRequest, TagsByFilterResponse]) -> None:
+        pass
+
+    async def AddBoundingBoxToImageByID(self, stream: Stream[AddBoundingBoxToImageByIDRequest, AddBoundingBoxToImageByIDResponse]) -> None:
+        pass
+
+    async def RemoveBoundingBoxFromImageByID(
+        self,
+        stream: Stream[RemoveBoundingBoxFromImageByIDRequest, RemoveBoundingBoxFromImageByIDResponse]
+    ) -> None:
+        pass
+
+    async def BoundingBoxLabelsByFilter(self, stream: Stream[BoundingBoxLabelsByFilterRequest, BoundingBoxLabelsByFilterResponse]) -> None:
+        pass
+
+    async def DataCaptureUpload(self, stream: Stream[DataCaptureUploadRequest, DataCaptureUploadResponse]) -> None:
+        return None
+
+    async def FileUpload(self, stream: Stream[FileUploadRequest, FileUploadResponse]) -> None:
+        pass
+
+
+async def main(*, host: str = '127.0.0.1', port: int = 9092) -> None:
+    server = Server([DataServer()])
+    with graceful_exit([server]):
+        await server.start(host, port)
+        await server.wait_closed()
+
+if __name__ == '__main__':
+    asyncio.run(main())
