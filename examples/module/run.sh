629229b4676ae7492a84d173311ff67490e79077
Cheuk Tse <cheuk@viam.com>
2023-07-19 13:11:22 -0400

    add venv stuff

--- examples/module/run.sh
+++ examples/module/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 cd `dirname $0`
 
+# Create a virtual environment to run our code
+VENV="venv"
+PYTHON="$VENV/bin/python"
+
+python3 -m venv $VENV
+PYTHON -m pip install -r requirements.txt -U # remove -U if viam-sdk should not be upgraded whenever possible
+
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec poetry run python -m src.main $@
+exec PYTHON -m src.main $@
