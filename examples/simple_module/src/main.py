875f3a9325c6bb6b3d7ae4d08f0e4631d84f4955
Cheuk Tse <cheuk@viam.com>
2023-07-22 15:24:18 -0400

    more updates

--- examples/simple_module/src/main.py
+++ examples/simple_module/src/main.py
@@ -33,8 +33,9 @@ class MySensor(Sensor):
 
 
 async def main():
-    """This function creates and starts a new module, after adding all desired resource models."""
-    # first register your new resource creator!
+    """This function creates and starts a new module, after adding all desired resource model.
+    Resource creators must be registered to the resource registry before the module adds the resource model.
+    """
     Registry.register_resource_creator(Sensor.SUBTYPE, MySensor.MODEL, ResourceCreatorRegistration(MySensor.new))
 
     module = Module.from_args()
