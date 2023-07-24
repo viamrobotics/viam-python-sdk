23cdb4945f335c56dadedb202f6e8aa649ce0392
Cheuk Tse <cheuk@viam.com>
2023-07-24 17:22:19 -0400

    change name

--- examples/simple_module/run.sh
+++ examples/simple_module/run.sh
@@ -2,10 +2,10 @@
 cd `dirname $0`
 
 # Create a virtual environment to run our code
-VENV="venv"
-PYTHON="$VENV/bin/python"
+VENV_NAME="venv"
+PYTHON="$VENV_NAME/bin/python"
 
-python3 -m venv $VENV
+python3 -m venv $VENV_NAME
 $PYTHON -m pip install -r requirements.txt -U # remove -U if viam-sdk should not be upgraded whenever possible
 
 # Be sure to use `exec` so that termination signals reach the python process,
