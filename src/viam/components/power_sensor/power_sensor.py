bb243006e4bf1c414f4fa4e5f6e075b63bf756a9
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2023-08-03 15:33:24 -0400

    RSDK-4056 Add PowerSensor (#374)

---
+++ src/viam/components/power_sensor/power_sensor.py
@@ -0,0 +1,92 @@
+import abc
+import asyncio
+from typing import Any, Dict, Final, List, Mapping, Optional, Tuple
+
+from grpclib import GRPCError
+
+from viam.errors import MethodNotImplementedError, NotSupportedError
+from viam.resource.types import RESOURCE_NAMESPACE_RDK, RESOURCE_TYPE_COMPONENT, Subtype
+
+from ..sensor import Sensor
+
+
+class PowerSensor(Sensor):
+    """PowerSensor reports information about voltage, current and power.
+
+    This acts as an abstract base class for any sensors that can provide data regarding voltage, current and/or power.
+    This cannot be used on its own. If the ``__init__()`` function is overridden, it must call the ``super().__init__()`` function.
+    """
+
+    SUBTYPE: Final = Subtype(RESOURCE_NAMESPACE_RDK, RESOURCE_TYPE_COMPONENT, "power_sensor")
+
+    @abc.abstractmethod
+    async def get_voltage(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None, **kwargs) -> Tuple[float, bool]:
+        """Get the voltage reading and bool IsAC
+
+        Returns:
+            Tuple[float, bool]: voltage (volts) and bool IsAC
+        """
+        ...
+
+    @abc.abstractmethod
+    async def get_current(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None, **kwargs) -> Tuple[float, bool]:
+        """Get the current reading and bool IsAC
+
+        Returns:
+            Tuple[float, bool]: current (amperes) and bool IsAC
+        """
+        ...
+
+    @abc.abstractmethod
+    async def get_power(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None, **kwargs) -> float:
+        """Get the power reading in watts
+
+        Returns:
+            float: power in watts
+        """
+        ...
+
+    async def get_readings(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None, **kwargs) -> Mapping[str, Any]:
+        """Obtain the measurements/data specific to this sensor.
+        If a sensor is not configured to have a measurement or fails to read a piece of data, it will not appear in the readings dictionary.
+
+        Returns:
+            Mapping[str, Any]: The readings for the PowerSensor:
+            {
+               voltage: float
+               current: float
+               is_ac: bool
+               power: float
+            }
+        """
+        (vol, cur, pow) = await asyncio.gather(
+            self.get_voltage(extra=extra, timeout=timeout),
+            self.get_current(extra=extra, timeout=timeout),
+            self.get_power(extra=extra, timeout=timeout),
+            return_exceptions=True,
+        )
+
+        readings = {}
+
+        # Add returned value to the readings dictionary if value is of expected type; omit if unimplemented.
+        def add_reading(name: str, reading, returntype: List) -> None:
+            possible_error_types = (NotImplementedError, MethodNotImplementedError, NotSupportedError)
+            if type(reading) in returntype:
+                if name == "voltage":
+                    readings["voltage"] = reading[0]
+                    readings["is_ac"] = reading[1]
+                elif name == "current":
+                    readings["current"] = reading[0]
+                    readings["is_ac"] = reading[1]
+                else:
+                    readings[name] = reading
+                return
+            elif isinstance(reading, possible_error_types) or (isinstance(reading, GRPCError) and "Unimplemented" in str(reading.message)):
+                return
+            raise reading
+
+        add_reading("voltage", vol, [tuple])
+        add_reading("current", cur, [tuple])
+        add_reading("power", pow, [float])
+
+        return readings
---
+++ src/viam/components/power_sensor/service.py
@@ -0,0 +1,62 @@
+from grpclib.server import Stream
+
+from viam.components.power_sensor.power_sensor import PowerSensor
+from viam.proto.common import DoCommandRequest, DoCommandResponse
+from viam.proto.component.powersensor import (
+    GetVoltageRequest,
+    GetVoltageResponse,
+    GetCurrentRequest,
+    GetCurrentResponse,
+    GetPowerRequest,
+    GetPowerResponse,
+    PowerSensorServiceBase,
+)
+from viam.resource.rpc_service_base import ResourceRPCServiceBase
+from viam.utils import dict_to_struct, struct_to_dict
+
+
+class PowerSensorRPCService(PowerSensorServiceBase, ResourceRPCServiceBase):
+    """
+    gRPC Service for a PowerSensor
+    """
+
+    RESOURCE_TYPE = PowerSensor
+
+    async def GetVoltage(self, stream: Stream[GetVoltageRequest, GetVoltageResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        sensor = self.get_resource(name)
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        voltage, is_ac = await sensor.get_voltage(extra=struct_to_dict(request.extra), timeout=timeout, metadata=stream.metadata)
+        response = GetVoltageResponse(volts=voltage, is_ac=is_ac)
+        await stream.send_message(response)
+
+    async def GetCurrent(self, stream: Stream[GetCurrentRequest, GetCurrentResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        sensor = self.get_resource(name)
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        current, is_ac = await sensor.get_current(extra=struct_to_dict(request.extra), timeout=timeout, metadata=stream.metadata)
+        response = GetCurrentResponse(amperes=current, is_ac=is_ac)
+        await stream.send_message(response)
+
+    async def GetPower(self, stream: Stream[GetPowerRequest, GetPowerResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        name = request.name
+        sensor = self.get_resource(name)
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        power = await sensor.get_power(extra=struct_to_dict(request.extra), timeout=timeout, metadata=stream.metadata)
+        response = GetPowerResponse(watts=power)
+        await stream.send_message(response)
+
+    async def DoCommand(self, stream: Stream[DoCommandRequest, DoCommandResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        sensor = self.get_resource(request.name)
+        timeout = stream.deadline.time_remaining() if stream.deadline else None
+        result = await sensor.do_command(command=struct_to_dict(request.command), timeout=timeout, metadata=stream.metadata)
+        response = DoCommandResponse(result=dict_to_struct(result))
+        await stream.send_message(response)
