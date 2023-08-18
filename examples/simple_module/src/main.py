a04bc305fb3aff76e735dfdbbb0ea0a276543e1e
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-08-18 17:10:56 -0400

    add validate_config and reconfigure_resource to simple module

--- examples/simple_module/src/main.py
+++ examples/simple_module/src/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Any, ClassVar, Dict, Mapping, Optional
+from typing import Any, ClassVar, Dict, Mapping, Optional, Sequence
 
 from typing_extensions import Self
 
@@ -16,6 +16,7 @@ from viam.utils import ValueTypes
 class MySensor(Sensor):
     # Subclass the Viam Sensor component and implement the required functions
     MODEL: ClassVar[Model] = Model(ModelFamily("acme", "demo"), "mysensor")
+    multiplier: float
 
     def __init__(self, name: str):
         super().__init__(name)
@@ -25,18 +26,33 @@ class MySensor(Sensor):
         sensor = cls(config.name)
         return sensor
 
+    @classmethod
+    def validate_config(cls, config: ComponentConfig) -> Sequence[]:
+        if "multiplier" in config.attributes.fields:
+            if not isinstance(config.attributes.fields["multiplier"], float):
+                raise Exception("Multiplier must be a float.")
+            cls.multiplier = config.attributes.fields["multiplier"].number_value
+            if cls.multiplier == 0:
+                raise Exception("Multiplier cannot be 0.")
+        else:
+            cls.multiplier = 1.0
+        return []
+
     async def get_readings(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> Mapping[str, Any]:
-        return {"signal": 1}
+        return {"signal": 1 * self.multiplier}
 
     async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None, **kwargs) -> Mapping[str, ValueTypes]:
         return command
+
+    def reconfigure_resource(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        self.multiplier = config.attributes.fields["multiplier"].number_value
 
 
 async def main():
     """This function creates and starts a new module, after adding all desired resource models.
     Resource creators must be registered to the resource registry before the module adds the resource model.
     """
-    Registry.register_resource_creator(Sensor.SUBTYPE, MySensor.MODEL, ResourceCreatorRegistration(MySensor.new))
+    Registry.register_resource_creator(Sensor.SUBTYPE, MySensor.MODEL, ResourceCreatorRegistration(MySensor.new, MySensor.validate_config))
 
     module = Module.from_args()
     module.add_model_from_registry(Sensor.SUBTYPE, MySensor.MODEL)
