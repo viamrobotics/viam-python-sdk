3c6b91f6da8d6de62d5bbe32e2f102c541243882
martha-johnston <martha.johnston@viam.com>
2023-08-22 09:51:31 -0400

    update protos and reformat

--- src/viam/components/base/client.py
+++ src/viam/components/base/client.py
@@ -119,9 +119,11 @@ class BaseClient(Base, ReconfigurableResourceRPCClientBase):
             extra = {}
         request = GetPropertiesRequest(name=self.name, extra=dict_to_struct(extra))
         response: GetPropertiesResponse = await self.client.GetProperties(request, timeout=timeout)
-        return Base.Properties(width_meters=response.width_meters,
-                               turning_radius_meters=response.turning_radius_meters,
-                               wheel_circumference_meters=response.wheel_circumference_meters)
+        return Base.Properties(
+            width_meters=response.width_meters,
+            turning_radius_meters=response.turning_radius_meters,
+            wheel_circumference_meters=response.wheel_circumference_meters,
+        )
 
     async def do_command(
         self,
