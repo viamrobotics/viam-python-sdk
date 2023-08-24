2ab72fffe0211b50e3c587b698cbbfbba7cf1078
Bashar Eid <bashar.eid@viam.com>
2023-08-24 12:37:34 -0400

    fix docstring

--- src/viam/app/data_client.py
+++ src/viam/app/data_client.py
@@ -185,8 +185,8 @@ class DataClient:
             include_file_data (bool): Boolean specifying whether to actually include the binary file data with each retrieved file. Defaults
                 to false (i.e., only the files' metadata is returned).
             num_files (Optional[str]): Number of binary data to return. Passing 0 returns all binary data matching the filter no matter.
-                Defaults to 100 if no binary data is requested, otherwise 10. All binary data or the first `num_binary_data` will be
-                returned, whichever comes first.
+                Defaults to 100 if no binary data is requested, otherwise 10. All binary data or the first `num_files` will be returned,
+                    whichever comes first.
 
         Raises:
             ValueError: If `num_files` is less than 0.
