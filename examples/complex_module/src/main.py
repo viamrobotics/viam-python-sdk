8659416b894b3ecafa9722617cbab08954e4cd50
Bashar <96631404+8ashar@users.noreply.github.com>
2023-08-29 12:22:36 -0400

    [RSDK 4187] Custom Base Example (#405)

--- examples/complex_module/src/main.py
+++ examples/complex_module/src/main.py
@@ -2,21 +2,23 @@ import asyncio
 
 from viam.module.module import Module
 from viam.components.arm import Arm
+from viam.components.base import Base
 
 from .arm.my_arm import MyArm
-from .gizmo import Gizmo, MyGizmo
-from .summation import MySummationService, SummationService
+from .base.my_base import MyBase
+from .gizmo.my_gizmo import Gizmo, MyGizmo
+from .summation.my_summation import SummationService, MySummationService
 
 
 async def main():
     """This function creates and starts a new module, after adding all desired resource models.
     Resource models must be pre-registered. For an example, see the `gizmo.__init__.py` file.
     """
-
     module = Module.from_args()
     module.add_model_from_registry(Gizmo.SUBTYPE, MyGizmo.MODEL)
     module.add_model_from_registry(SummationService.SUBTYPE, MySummationService.MODEL)
     module.add_model_from_registry(Arm.SUBTYPE, MyArm.MODEL)
+    module.add_model_from_registry(Base.SUBTYPE, MyBase.MODEL)
     await module.start()
 
 
