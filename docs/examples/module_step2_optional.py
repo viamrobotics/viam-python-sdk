4ccdc1ba365ef4a7dc30dff7fc86e2523befb9c1
Nicole Jung <31713368+purplenicole730@users.noreply.github.com>
2023-08-29 09:28:56 -0400

    RSDK-4172 - update wifi sensor (#393)

--- docs/examples/module_step2_optional.py
+++ docs/examples/module_step2_optional.py
@@ -1,25 +1,20 @@
+# wifi-sensor/src/wifi_sensor_module.py
 import asyncio
-from typing import Any, ClassVar, Dict, Mapping, Optional, Sequence
-
+from typing import Any, ClassVar, Dict, List, Mapping, Optional, Sequence
 from typing_extensions import Self
 
-from viam.components.sensor import Sensor
-from viam.module.module import Module
+from viam.components.sensor import Geometry, Sensor
 from viam.proto.app.robot import ComponentConfig
 from viam.proto.common import ResourceName
 from viam.resource.base import ResourceBase
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from viam.resource.types import Model, ModelFamily
-from viam.utils import ValueTypes
 
 
 class MySensor(Sensor):
     # Subclass the Viam Sensor component and implement the required functions
-    MODEL: ClassVar[Model] = Model(ModelFamily("acme", "demo"), "mysensor")
+    MODEL: ClassVar[Model] = Model(ModelFamily("acme", "wifi_sensor"), "linux")
     multiplier: float
-
-    def __init__(self, name: str):
-        super().__init__(name)
 
     @classmethod
     def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
@@ -39,24 +34,24 @@ class MySensor(Sensor):
         return [""]
 
     async def get_readings(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> Mapping[str, Any]:
-        return {"signal": 1 * self.multiplier}
+        with open("/proc/net/wireless") as wifi_stats:
+            content = wifi_stats.readlines()
+        result = [x for x in content[2].split(" ") if x != ""]
 
-    async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None, **kwargs) -> Mapping[str, ValueTypes]:
-        return command
+        wifi_signal = []
+        for i in range(2, 5):
+            wifi_signal.append(int(result[i]) * self.multiplier)
+        return {"link": wifi_signal[0], "level": wifi_signal[1], "noise": wifi_signal[2]}
+
+    async def get_geometries(self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None) -> List[Geometry]:
+        raise NotImplementedError
 
     def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
         self.multiplier = config.attributes.fields["multiplier"].number_value
 
 
 async def main():
-    """This function creates and starts a new module, after adding all desired resource models.
-    Resource creators must be registered to the resource registry before the module adds the resource model.
-    """
     Registry.register_resource_creator(Sensor.SUBTYPE, MySensor.MODEL, ResourceCreatorRegistration(MySensor.new, MySensor.validate_config))
-
-    module = Module.from_args()
-    module.add_model_from_registry(Sensor.SUBTYPE, MySensor.MODEL)
-    await module.start()
 
 
 if __name__ == "__main__":
