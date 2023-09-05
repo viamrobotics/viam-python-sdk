f1d778b9ad64882b5aa0f45b5e3cd6a5657e9731
Cheuk Tse <cheuk@viam.com>
2023-09-05 17:30:05 -0400

    update

--- examples/complex_module/run.sh
+++ examples/complex_module/run.sh
@@ -5,9 +5,37 @@ cd `dirname $0`
 VENV_NAME="venv"
 PYTHON="$VENV_NAME/bin/python"
 
-python3 -m venv $VENV_NAME
-$PYTHON -m pip install -r requirements.txt -U # remove -U if viam-sdk should not be upgraded whenever possible
+if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+    echo "Failed to create virtualenv."
+    if command -v apt-get >/dev/null; then
+        echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
+        SUDO="sudo"
+        if ! command -v $SUDO >/dev/null; then
+            SUDO=""
+        fi
+		if ! apt info python3-venv >/dev/null 2>&1; then
+			echo "package info not found, trying apt update"
+			$SUDO apt -qq update >/dev/null
+		fi
+        $SUDO apt install -qqy python3-venv >/dev/null 2>&1
+        if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+            echo "This module requires Python >=3.8 and pip to be installed." >&2
+            exit 1
+        fi
+    else
+        echo "This module requires Python >=3.8 and pip to be installed." >&2
+        exit 1
+    fi
+fi
+
+# remove -U if viam-sdk should not be upgraded whenever possible
+# -qq suppresses extraneous output from pip
+echo "Virtualenv found/created. Installing/upgrading Python packages..."
+if ! $PYTHON -m pip install -r requirements.txt -Uqq; then
+    exit 1
+fi
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
+echo "Starting module..."
 exec $PYTHON -m src.main $@
