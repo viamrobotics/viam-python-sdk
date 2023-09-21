d91570a0c1768e1d46d6794f01c45f20ef7dfc5d
Cheuk Tse <cheuk@viam.com>
2023-09-21 16:06:54 -0400

    flyby

--- src/viam/utils.py
+++ src/viam/utils.py
@@ -259,7 +259,7 @@ async def to_thread(func: Callable[_P, _R], *args: _P.args, **kwargs: _P.kwargs)
 
 def from_dm_from_extra(extra: Optional[Dict[str, Any]]) -> bool:
     """Used in modular filter components to get the 'fromDataManagement' value from an extra map."""
-    if extra is None or "fromDataManagement" not in extra:
+    if extra is None:
         return False
 
     return bool(extra.get("fromDataManagement", False))
