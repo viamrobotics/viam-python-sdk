ff1eac69a43daac2900b20a8449ed8a89b7f1b1e
Ethan Rodkin <ethan@viam.com>
2023-10-24 11:14:33 -0400

    shells of methods, commit before waiting for proto updates

--- src/viam/app/billing_client.py
+++ src/viam/app/billing_client.py
@@ -11,6 +11,18 @@ from viam.proto.app.billing import (
         GetCurrentMonthUsageResponse,
         GetCurrentMonthUsageSummaryRequest,
         GetCurrentMonthUsageSummaryResponse,
+        GetInvoiceHistoryRequest,
+        GetInvoiceHistoryResponse,
+        GetInvoicePdfRequest,
+        GetInvoicePdfResponse,
+        GetInvoicesSummaryRequest,
+        GetInvoicesSummaryResponse,
+        GetItemizedInvoiceRequest,
+        GetItemizedInvoiceResponse,
+        GetOrgBillingInformationRequest,
+        GetOrgBillingInformationResponse,
+        GetUnpaidBalanceRequest,
+        GetUnpaidBalanceResponse,
 )
 
 LOGGER = logging.getLogger(__name__)
@@ -38,6 +50,7 @@ class BillingClient:
     _channel: Channel
     _metadata: Mapping[str, str]
 
+    # CR erodkin: add timeouts
     async def get_billing_summary(self, org_id: str) -> GetBillingSummaryResponse:
         request = GetBillingSummaryRequest(org_id=org_id)
         return await self._billing_client.GetBillingSummary(request, metadata=self._metadata)
@@ -50,21 +63,27 @@ class BillingClient:
         request = GetCurrentMonthUsageRequest(org_id=org_id)
         return await self._billing_client.GetCurrentMonthUsageSummary(request, metadata=self._metadata)
 
-    async def get_invoice_history(self) -> None:
-        pass
+    async def get_invoice_history(self, org_id: str) -> GetInvoiceHistoryResponse:
+        request = GetInvoiceHistoryRequest(org_id=org_id)
+        return await self._billing_client.GetInvoiceHistory(request, metadata=self._metadata)
 
-    async def get_invoice_pdf(self) -> None:
-        pass
+    async def get_invoice_pdf(self, id: str, org_id: str) -> GetInvoicePdfResponse:
+        request = GetInvoicePdfRequest(id=id, org_id=org_id)
+        return await self._billing_client.GetInvoicePdf(request, metadata=self._metadata)
 
-    async def get_invoices_summary(self) -> None:
-        pass
+    async def get_invoices_summary(self, org_id: str) -> GetInvoicesSummaryResponse:
+        request = GetInvoicesSummaryRequest(org_id=org_id)
+        return await self._billing_client.GetInvoicesSummary(request, metadata=self._metadata)
 
-    async def get_itemized_invoice(self) -> None:
-        pass
+    async def get_itemized_invoice(self, id: str) -> GetItemizedInvoiceResponse:
+        request = GetItemizedInvoiceRequest(id=id)
+        return await self._billing_client.GetItemizedInvoice(request, metadata=self._metadata)
 
-    async def get_org_billing_information(self) -> None:
-        pass
+    async def get_org_billing_information(self, org_id: str) -> GetOrgBillingInformationResponse:
+        request = GetOrgBillingInformationRequest(org_id=org_id)
+        return await self._billing_client.GetOrgBillingInformation(request, metadata=self._metadata)
 
-    async def get_unpaid_balance(self) -> None:
-        pass
+    async def get_unpaid_balance(self, org_id: str) -> GetUnpaidBalanceResponse:
+        request = GetUnpaidBalanceRequest(org_id=org_id)
+        return await self._billing_client.GetUnpaidBalance(request, metadata=self._metadata)
 
