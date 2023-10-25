709db5ee2170e52f971137a359ba7aee4398f9d9
Ethan Rodkin <ethan@viam.com>
2023-10-25 12:14:43 -0400

    add tests

--- tests/mocks/services.py
+++ tests/mocks/services.py
@@ -211,6 +211,17 @@ from viam.proto.app.mltraining import (
     SubmitTrainingJobResponse,
     TrainingJobMetadata,
 )
+from viam.proto.app.billing import (
+    BillingServiceBase,
+    GetCurrentMonthUsageRequest,
+    GetCurrentMonthUsageResponse,
+    GetInvoicePdfRequest,
+    GetInvoicePdfResponse,
+    GetInvoicesSummaryRequest,
+    GetInvoicesSummaryResponse,
+    GetOrgBillingInformationRequest,
+    GetOrgBillingInformationResponse,
+)
 from viam.proto.common import DoCommandRequest, DoCommandResponse, GeoObstacle, GeoPoint, PointCloudObject, Pose, PoseInFrame, ResourceName
 from viam.proto.service.mlmodel import (
     FlatTensor,
@@ -816,6 +827,46 @@ class MockMLTraining(MLTrainingServiceBase):
         assert request is not None
         self.cancel_job_id = request.id
         await stream.send_message(CancelTrainingJobResponse())
+
+
+class MockBilling(BillingServiceBase):
+    def __init__(
+        self,
+        pdf: bytes,
+        curr_month_usage: GetCurrentMonthUsageResponse,
+        invoices_summary: GetInvoicesSummaryResponse,
+        billing_info: GetOrgBillingInformationResponse,
+    ):
+        self.pdf = pdf
+        self.curr_month_usage = curr_month_usage
+        self.invoices_summary = invoices_summary
+        self.billing_info = billing_info
+
+    async def GetCurrentMonthUsage(self, stream: Stream[GetCurrentMonthUsageRequest, GetCurrentMonthUsageResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        self.org_id = request.org_id
+        await stream.send_message(self.curr_month_usage)
+
+    async def GetInvoicePdf(self, stream: Stream[GetInvoicePdfRequest, GetInvoicePdfResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        self.org_id = request.org_id
+        self.invoice_id = request.id
+        response = GetInvoicePdfResponse(chunk=self.pdf)
+        await stream.send_message(response)
+
+    async def GetInvoicesSummary(self, stream: Stream[GetInvoicesSummaryRequest, GetInvoicePdfResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        self.org_id = request.org_id
+        await stream.send_message(self.invoice_summary)
+
+    async def GetOrgBillingInformation(self, stream: Stream[GetOrgBillingInformationRequest, GetOrgBillingInformationResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+        self.org_id = request.org_id
+        await stream.send_message(self.billing_info)
 
 
 class MockApp(AppServiceBase):
---
+++ tests/test_billing_client.py
@@ -0,0 +1,115 @@
+import pytest
+
+from google.protobuf.timestamp_pb2 import Timestamp
+from grpclib.testing import ChannelFor
+
+from viam.app.billing_client import BillingClient
+from viam.proto.app.billing import (
+    GetCurrentMonthUsageResponse,
+    GetInvoicesSummaryResponse,
+    GetOrgBillingInformationResponse,
+    InvoiceSummary,
+    PaymentMethodType,
+)
+
+from .mocks.services import MockBilling
+
+# CR erodkin: probably don't need PDF unless we change the GetInvoicePdf api
+PDF = b'abc123'
+CLOUD_STORAGE_USAGE_COST = 100.0
+DATA_UPLOAD_USAGE_COST = 101.0
+DATA_EGRES_USAGE_COST = 102.0
+REMOTE_CONTROL_USAGE_COST = 103.0
+STANDARD_COMPUTE_USAGE_COST = 104.0
+DISCOUNT_AMOUNT = 0.0
+TOTAL_USAGE_WITH_DISCOUNT = 105.0
+TOTAL_USAGE_WITHOUT_DISCOUNT = 106.0
+OUTSTANDING_BALANCE = 1000.0
+SECONDS_START = 978310861
+NANOS_START = 0
+SECONDS_END = 998310861
+NANOS_END = 0
+SECONDS_PAID = 988310861
+NANOS_PAID = 0
+START_TS = Timestamp(seconds=SECONDS_START, nanos=NANOS_END)
+PAID_DATE_TS = Timestamp(seconds=SECONDS_PAID, nanos=NANOS_PAID)
+END_TS = Timestamp(seconds=SECONDS_END, nanos=NANOS_END)
+INVOICE_ID = "invoice"
+STATUS = "status"
+PAYMENT_TYPE = 1
+EMAIL = "email@fake.com"
+BILLING_TIER = "tier"
+INVOICE = InvoiceSummary(
+    id=INVOICE_ID,
+    invoice_date=START_TS,
+    invoice_amount=OUTSTANDING_BALANCE,
+    status=STATUS,
+    due_date=END_TS,
+    paid_date=PAID_DATE_TS,
+)
+INVOICES = [INVOICE]
+CURR_MONTH_USAGE = GetCurrentMonthUsageResponse(
+    start_date=START_TS,
+    end_date=END_TS,
+    cloud_storage_usage_cost=CLOUD_STORAGE_USAGE_COST,
+    data_upload_usage_cost=DATA_UPLOAD_USAGE_COST,
+    data_egres_usage_cost=DATA_EGRES_USAGE_COST,
+    remote_control_usage_cost=REMOTE_CONTROL_USAGE_COST,
+    standard_compute_usage_cost=STANDARD_COMPUTE_USAGE_COST,
+    discount_amount=DISCOUNT_AMOUNT,
+    total_usage_with_discount=TOTAL_USAGE_WITH_DISCOUNT,
+    total_usage_without_discount=TOTAL_USAGE_WITHOUT_DISCOUNT,
+)
+INVOICES_SUMMARY = GetInvoicesSummaryResponse(outstanding_balance=OUTSTANDING_BALANCE, invoices=INVOICES)
+ORG_BILLING_INFO = GetOrgBillingInformationResponse(
+    type=PAYMENT_TYPE,
+    billing_email=EMAIL,
+    billing_tier=BILLING_TIER,
+)
+
+AUTH_TOKEN = "auth_token"
+BILLING_SERVICE_METADATA = {"authorization": f"Bearer {AUTH_TOKEN}"}
+
+@pytest.fixture(scope="function")
+def service() -> MockBilling:
+    return MockBilling(
+        pdf=PDF,
+        curr_month_usage=CURR_MONTH_USAGE,
+        invoice_summary=INVOICES_SUMMARY,
+        billing_info=ORG_BILLING_INFO,
+    )
+
+
+class TestClient:
+    @pytest.mark.asyncio
+    async def test_get_current_month_usage(self, service: MockBilling):
+        async with ChannelFor([service]) as channel:
+            org_id = "foo"
+            client = BillingClient(channel, BILLING_SERVICE_METADATA)
+            curr_month_usage = await client.get_current_month_usage(org_id=org_id)
+            assert curr_month_usage == CURR_MONTH_USAGE
+            assert service.org_id == org_id
+
+    @pytest.mark.asyncio
+    async def test_get_invoice_pdf(self, service: MockBilling):
+        assert True
+
+    @pytest.mark.asyncio
+    async def test_get_invoices_summary(self, service: MockBilling):
+        async with ChannelFor([service]) as channel:
+            org_id = "bar"
+            client = BillingClient(channel, BILLING_SERVICE_METADATA)
+            invoices_summary = await client.get_invoices_summary(org_id=org_id)
+            assert invoices_summary == INVOICES_SUMMARY
+            assert service.org_id == org_id
+
+    @pytest.mark.asyncio
+    async def test_get_org_billing_information(self, service: MockBilling):
+        async with ChannelFor([service]) as channel:
+            org_id = "baz"
+            client = BillingClient(channel, BILLING_SERVICE_METADATA)
+            org_billing_info = await client.get_org_billing_information(org_id=org_id)
+            assert org_billing_info == ORG_BILLING_INFO
+            assert service.org_id == org_id
+
+
