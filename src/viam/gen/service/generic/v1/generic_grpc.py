0d7823c2b596e1d0f6e5c5e8ab476082a4b8b90d
viambot <viambot@users.noreply.github.com>
2024-01-18 17:19:56 +0000

    [WORKFLOW] Updating protos from viamrobotics/api, commit: 56f5cb47cf60eabc9b96bc0ad22a2a2c239ba058

---
+++ src/viam/gen/service/generic/v1/generic_grpc.py
@@ -0,0 +1,23 @@
+import abc
+import typing
+import grpclib.const
+import grpclib.client
+if typing.TYPE_CHECKING:
+    import grpclib.server
+from .... import common
+import google.api.annotations_pb2
+from .... import service
+
+class GenericServiceBase(abc.ABC):
+
+    @abc.abstractmethod
+    async def DoCommand(self, stream: 'grpclib.server.Stream[common.v1.common_pb2.DoCommandRequest, common.v1.common_pb2.DoCommandResponse]') -> None:
+        pass
+
+    def __mapping__(self) -> typing.Dict[str, grpclib.const.Handler]:
+        return {'/viam.service.generic.v1.GenericService/DoCommand': grpclib.const.Handler(self.DoCommand, grpclib.const.Cardinality.UNARY_UNARY, common.v1.common_pb2.DoCommandRequest, common.v1.common_pb2.DoCommandResponse)}
+
+class GenericServiceStub:
+
+    def __init__(self, channel: grpclib.client.Channel) -> None:
+        self.DoCommand = grpclib.client.UnaryUnaryMethod(channel, '/viam.service.generic.v1.GenericService/DoCommand', common.v1.common_pb2.DoCommandRequest, common.v1.common_pb2.DoCommandResponse)
