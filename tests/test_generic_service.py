7064b425836376699fff5e1ada0ecc23ddc191e4
Benji Rewis <benji.rewis@viam.com>
2024-01-19 12:11:37 -0500

    RSDK-6375 Add generic service wrappers

--- tests/test_generic_service.py
+++ tests/test_generic_service.py
@@ -2,17 +2,17 @@ import pytest
 from grpclib.testing import ChannelFor
 
 from viam.components.generic import GenericClient, GenericRPCService
-from viam.proto.common import DoCommandRequest, DoCommandResponse, GetGeometriesRequest, GetGeometriesResponse
+from viam.proto.common import DoCommandRequest, DoCommandResponse
 from viam.proto.component.generic import GenericServiceStub
 from viam.resource.manager import ResourceManager
 from viam.utils import dict_to_struct, struct_to_dict
 
 from . import loose_approx
-from .mocks.components import GEOMETRIES, MockGeneric
+from .mocks.services import MockGenericService
 
 
-class TestGeneric:
-    generic = MockGeneric(name="generic")
+class TestGenericService:
+    generic = MockGenericService(name="generic")
 
     @pytest.mark.asyncio
     async def test_do(self):
@@ -20,17 +20,12 @@ class TestGeneric:
         assert result == {"command": True}
         assert self.generic.timeout == loose_approx(1.82)
 
-    @pytest.mark.asyncio
-    async def test_get_geometries(self):
-        geometries = await self.generic.get_geometries()
-        assert geometries == GEOMETRIES
-
 
 class TestService:
     @classmethod
     def setup_class(cls):
         cls.name = "generic"
-        cls.generic = MockGeneric(name=cls.name)
+        cls.generic = MockGenericService(name=cls.name)
         cls.manager = ResourceManager([cls.generic])
         cls.service = GenericRPCService(cls.manager)
 
@@ -44,20 +39,12 @@ class TestService:
             assert result == {"command": True}
             assert self.generic.timeout == loose_approx(4.4)
 
-    @pytest.mark.asyncio
-    async def test_get_geometries(self):
-        async with ChannelFor([self.service]) as channel:
-            client = GenericServiceStub(channel)
-            request = GetGeometriesRequest(name=self.name)
-            response: GetGeometriesResponse = await client.GetGeometries(request)
-            assert [geometry for geometry in response.geometries] == GEOMETRIES
-
 
 class TestClient:
     @classmethod
     def setup_class(cls):
         cls.name = "generic"
-        cls.generic = MockGeneric(name=cls.name)
+        cls.generic = MockGenericService(name=cls.name)
         cls.manager = ResourceManager([cls.generic])
         cls.service = GenericRPCService(cls.manager)
 
@@ -68,10 +55,3 @@ class TestClient:
             result = await client.do_command({"command": "args"}, timeout=7.86)
             assert result == {"command": True}
             assert self.generic.timeout == loose_approx(7.86)
-
-    @pytest.mark.asyncio
-    async def test_get_geometries(self):
-        async with ChannelFor([self.service]) as channel:
-            client = GenericClient(self.name, channel)
-            geometries = await client.get_geometries()
-            assert geometries == GEOMETRIES
