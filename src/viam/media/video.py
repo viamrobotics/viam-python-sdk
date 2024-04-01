58e4c2a160464b02a062dcc4b945bd714850562c
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-04-01 14:36:28 -0400

    remove rawimage

--- src/viam/media/video.py
+++ src/viam/media/video.py
@@ -1,7 +1,7 @@
 from array import array
 from enum import Enum
 from io import BytesIO
-from typing import List, NamedTuple, Optional, Union
+from typing import List, Optional, Union
 
 from PIL import Image, UnidentifiedImageError
 from typing_extensions import Self
@@ -13,51 +13,6 @@ from .viam_rgba_plugin import RGBA_FORMAT_LABEL
 
 # Formats that are supported by PIL
 LIBRARY_SUPPORTED_FORMATS = ["JPEG", "PNG", RGBA_FORMAT_LABEL]
-
-
-class RawImage(NamedTuple):
-    """**DEPRECATED** Use ``ViamImage`` instead
-
-    A raw bytes representation of an image.
-
-    A RawImage should be returned instead of a PIL Image instance under one of
-    the following conditions
-
-    1) The requested mime type has the LAZY_SUFFIX string appended to it
-    2) The requested mime type is not supported for decoding/encoding by Viam's
-    Python SDK
-    """
-
-    data: bytes
-    """The raw data of the image"""
-
-    mime_type: str
-    """The mimetype of the image"""
-
-    def close(self):
-        """Close the image and release resources. For RawImage, this is a noop."""
-        return
-
-    def bytes_to_depth_array(self) -> List[List[int]]:
-        """Decode the data of an image that has the custom depth MIME type ``image/vnd.viam.dep`` into
-        a standard representation.
-
-        Raises:
-            NotSupportedError: Raised if given an image that is not of MIME type `image/vnd.viam.dep`.
-
-        Returns:
-            List[List[int]]: The standard representation of the image.
-        """
-        if self.mime_type != CameraMimeType.VIAM_RAW_DEPTH.value:
-            raise NotSupportedError("Type must be `image/vnd.viam.dep` to use bytes_to_depth_array()")
-
-        width = int.from_bytes(self.data[8:16], "big")
-        height = int.from_bytes(self.data[16:24], "big")
-        depth_arr = array("H", self.data[24:])
-        depth_arr.byteswap()
-
-        depth_arr_2d = [[depth_arr[row * width + col] for col in range(width)] for row in range(height)]
-        return depth_arr_2d
 
 
 class CameraMimeType(str, Enum):
