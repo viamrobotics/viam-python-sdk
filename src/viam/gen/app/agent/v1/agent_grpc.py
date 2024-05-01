585558aac93d20879db2b3dce3de3ae8d676c9c3
viambot <viambot@users.noreply.github.com>
2024-05-01 16:05:55 +0000

    [WORKFLOW] Updating protos from viamrobotics/api, commit: b39c74b5a2d320fa459ef0ec4fac6a3b36a690e3

--- src/viam/gen/app/agent/v1/agent_grpc.py
+++ src/viam/gen/app/agent/v1/agent_grpc.py
@@ -7,35 +7,7 @@ if typing.TYPE_CHECKING:
     import grpclib.server
 import google.protobuf.duration_pb2
 import google.protobuf.struct_pb2
-from .... import tagger
 from .... import app
-
-class AgentAppServiceBase(abc.ABC):
-
-    @abc.abstractmethod
-    async def GetAgentConfig(self, stream: 'grpclib.server.Stream[app.agent.v1.agent_pb2.GetAgentConfigRequest, app.agent.v1.agent_pb2.GetAgentConfigResponse]') -> None:
-        pass
-
-    @abc.abstractmethod
-    async def UpdateAgentConfig(self, stream: 'grpclib.server.Stream[app.agent.v1.agent_pb2.UpdateAgentConfigRequest, app.agent.v1.agent_pb2.UpdateAgentConfigResponse]') -> None:
-        pass
-
-    def __mapping__(self) -> typing.Dict[str, grpclib.const.Handler]:
-        return {'/viam.app.agent.v1.AgentAppService/GetAgentConfig': grpclib.const.Handler(self.GetAgentConfig, grpclib.const.Cardinality.UNARY_UNARY, app.agent.v1.agent_pb2.GetAgentConfigRequest, app.agent.v1.agent_pb2.GetAgentConfigResponse), '/viam.app.agent.v1.AgentAppService/UpdateAgentConfig': grpclib.const.Handler(self.UpdateAgentConfig, grpclib.const.Cardinality.UNARY_UNARY, app.agent.v1.agent_pb2.UpdateAgentConfigRequest, app.agent.v1.agent_pb2.UpdateAgentConfigResponse)}
-
-class UnimplementedAgentAppServiceBase(AgentAppServiceBase):
-
-    async def GetAgentConfig(self, stream: 'grpclib.server.Stream[app.agent.v1.agent_pb2.GetAgentConfigRequest, app.agent.v1.agent_pb2.GetAgentConfigResponse]') -> None:
-        raise grpclib.exceptions.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
-
-    async def UpdateAgentConfig(self, stream: 'grpclib.server.Stream[app.agent.v1.agent_pb2.UpdateAgentConfigRequest, app.agent.v1.agent_pb2.UpdateAgentConfigResponse]') -> None:
-        raise grpclib.exceptions.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
-
-class AgentAppServiceStub:
-
-    def __init__(self, channel: grpclib.client.Channel) -> None:
-        self.GetAgentConfig = grpclib.client.UnaryUnaryMethod(channel, '/viam.app.agent.v1.AgentAppService/GetAgentConfig', app.agent.v1.agent_pb2.GetAgentConfigRequest, app.agent.v1.agent_pb2.GetAgentConfigResponse)
-        self.UpdateAgentConfig = grpclib.client.UnaryUnaryMethod(channel, '/viam.app.agent.v1.AgentAppService/UpdateAgentConfig', app.agent.v1.agent_pb2.UpdateAgentConfigRequest, app.agent.v1.agent_pb2.UpdateAgentConfigResponse)
 
 class AgentDeviceServiceBase(abc.ABC):
 
