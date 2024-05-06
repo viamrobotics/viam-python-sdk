96ef235fb6035bc39feb83ce670177cfedd9dcc4
Ethan <ethan@viam.com>
2024-05-06 12:58:25 -0400

    RSDK-7393 - remove references to do_command from mlmodel service (#604)

--- src/viam/services/mlmodel/client.py
+++ src/viam/services/mlmodel/client.py
@@ -1,13 +1,11 @@
-from typing import Dict, Mapping, Optional
+from typing import Dict, Optional
 
 from grpclib.client import Channel
 from numpy.typing import NDArray
 
-from viam.proto.common import DoCommandRequest, DoCommandResponse
 from viam.proto.service.mlmodel import InferRequest, InferResponse, MetadataRequest, MetadataResponse, MLModelServiceStub
 from viam.resource.rpc_client_base import ReconfigurableResourceRPCClientBase
 from viam.services.mlmodel.utils import flat_tensors_to_ndarrays, ndarrays_to_flat_tensors
-from viam.utils import ValueTypes, dict_to_struct, struct_to_dict
 
 from .mlmodel import Metadata, MLModel
 
@@ -27,8 +25,3 @@ class MLModelClient(MLModel, ReconfigurableResourceRPCClientBase):
         request = MetadataRequest(name=self.name)
         response: MetadataResponse = await self.client.Metadata(request)
         return response.metadata
-
-    async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None, **kwargs) -> Mapping[str, ValueTypes]:
-        request = DoCommandRequest(name=self.name, command=dict_to_struct(command))
-        response: DoCommandResponse = await self.client.DoCommand(request, timeout=timeout)
-        return struct_to_dict(response.result)
