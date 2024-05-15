c2e891b95a8aa01e2876a5e4da2dc53882e16887
Nick Franczak <nicholasgfranczak@gmail.com>
2024-05-15 16:22:18 -0400

    replace geoObstacle with geoGeometry + add argument to MoveOnGlobe in motion service

--- src/viam/services/navigation/__init__.py
+++ src/viam/services/navigation/__init__.py
@@ -1,4 +1,4 @@
-from viam.proto.common import GeoObstacle, GeoPoint
+from viam.proto.common import GeoGeometry, GeoPoint
 from viam.proto.service.navigation import MapType, Mode, Path, Waypoint
 from viam.resource.registry import Registry, ResourceRegistration
 
@@ -6,6 +6,6 @@ from .client import NavigationClient
 from .navigation import Navigation
 from .service import NavigationRPCService
 
-__all__ = ["GeoPoint", "GeoObstacle", "NavigationClient", "Navigation", "Waypoint", "Mode", "Path", "MapType"]
+__all__ = ["GeoPoint", "GeoGeometry", "NavigationClient", "Navigation", "Waypoint", "Mode", "Path", "MapType"]
 
 Registry.register_subtype(ResourceRegistration(Navigation, NavigationRPCService, lambda name, channel: NavigationClient(name, channel)))
