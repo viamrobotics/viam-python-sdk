5cf9c71233dc1fd7a8318a41bfab3af224f98535
Abe Winter <abe-winter@users.noreply.github.com>
2024-06-14 17:43:50 -0400

    checkpoint stubbed motor example (give it own file pls though)

--- examples/easy_resource/main.py
+++ examples/easy_resource/main.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
+#!../../.direnv/python-3.10.12/bin/python
+# #!/usr/bin/env python3
 
 import asyncio
 from viam.components.sensor import Sensor
-from viam.resource.easy_resource import EasyResource
+from viam.components.motor import Motor
+from viam.resource.easy_resource import EasyResource, stub_model
 from viam.module.module import Module
 
 
@@ -11,6 +13,17 @@ class MySensor(Sensor, EasyResource):
 
     async def get_readings(self, **kwargs):
         return {"ok": True}
+
+
+@stub_model
+class MyMotor(Motor, EasyResource):
+    MODEL = "viam:motor:easy-resource-example"
+
+    async def is_moving(self) -> bool:
+        return False
+
+    async def stop(self, **kwargs):
+        pass
 
 
 if __name__ == '__main__':
