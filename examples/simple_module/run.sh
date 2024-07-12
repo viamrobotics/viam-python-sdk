028e7e79da82cb02cbaf7b0d58c2e2e528b3ca47
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-07-12 16:55:41 -0400

    get outputs from shell script

--- examples/simple_module/run.sh
+++ examples/simple_module/run.sh
@@ -6,7 +6,7 @@ VENV_NAME="venv"
 PYTHON="$VENV_NAME/bin/python"
 ENV_ERROR="This module requires Python >=3.8, pip, and virtualenv to be installed."
 
-if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+if ! python3 -m venv $VENV_NAME; then
     echo "Failed to create virtualenv."
     if command -v apt-get >/dev/null; then
         echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
@@ -14,11 +14,11 @@ if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
         if ! command -v $SUDO >/dev/null; then
             SUDO=""
         fi
-		if ! apt info python3-venv >/dev/null 2>&1; then
+		if ! apt info python3-venv; then
 			echo "Package info not found, trying apt update"
 			$SUDO apt -qq update >/dev/null
 		fi
-        $SUDO apt install -qqy python3-venv >/dev/null 2>&1
+        $SUDO apt install -qqy python3-venv
         if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
             echo $ENV_ERROR >&2
             exit 1
