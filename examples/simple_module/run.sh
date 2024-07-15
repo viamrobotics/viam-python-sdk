e1060083325e4db43a7cdd5caadebd5edc3a7d0c
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-07-15 11:51:17 -0400

    isolate error

--- examples/simple_module/run.sh
+++ examples/simple_module/run.sh
@@ -6,7 +6,7 @@ VENV_NAME="venv"
 PYTHON="$VENV_NAME/bin/python"
 ENV_ERROR="This module requires Python >=3.8, pip, and virtualenv to be installed."
 
-if ! python3 -m venv $VENV_NAME; then
+if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
     echo "Failed to create virtualenv."
     if command -v apt-get >/dev/null; then
         echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
@@ -14,12 +14,12 @@ if ! python3 -m venv $VENV_NAME; then
         if ! command -v $SUDO >/dev/null; then
             SUDO=""
         fi
-		if ! apt info python3-venv; then
+		if ! apt info python3-venv >/dev/null 2>&1; then
 			echo "Package info not found, trying apt update"
 			$SUDO apt -qq update >/dev/null
 		fi
         $SUDO apt install -qqy python3-venv
-        if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+        if ! python3 -m venv $VENV_NAME; then
             echo $ENV_ERROR >&2
             exit 1
         fi
