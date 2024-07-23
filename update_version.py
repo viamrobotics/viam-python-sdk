3606e85c044c4d7221ffbb01bcf40fe20ec83fc8
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-07-23 14:20:52 -0400

    test locally

--- update_version.py
+++ update_version.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 
 file_path = "pyproject.toml"
@@ -16,3 +17,14 @@ def update_version(new_version: str):
 
     with open(file_path, 'w') as file:
         file.write(data)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python update_version.py <new_version>")
+        sys.exit(1)
+
+    new_version = sys.argv[1]
+
+    update_version(new_version)
+    print(f"Updated version to {new_version} in {file_path}")
