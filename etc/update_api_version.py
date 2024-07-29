96b94a7e6b7cc1d49be9d9055f74a780d4fad67b
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-07-29 11:04:53 -0400

    specify lowercase for version type

--- etc/update_api_version.py
+++ etc/update_api_version.py
@@ -6,21 +6,15 @@ def update_version(file_path: str, version_type: str, new_version: str):
     with open(file_path, "r") as file:
         data = file.read()
 
-    if version_type == "api":
-        data = re.sub(
-            r'(API_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"',
-            f'\\1"{new_version}"',
-            data
-        )
-    elif version_type == "sdk":
-        data = re.sub(
-            r'(SDK_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"',
-            f'\\1"{new_version}"',
-            data
-        )
+    if version_type.lower() == "api":
+        version = r'(API_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"'
+    elif version_type.lower() == "sdk":
+        version = r'(SDK_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"'
     else:
         print("version_type must be either 'api' or 'sdk'")
         sys.exit(1)
+
+    data = re.sub(version, f'\\1"{new_version}"', data)
 
     with open(file_path, 'w') as file:
         file.write(data)
