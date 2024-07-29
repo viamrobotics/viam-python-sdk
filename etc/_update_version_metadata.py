ed2c43db588b1ad510088115157fb5bc5ac5bd11
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-07-29 12:37:01 -0400

    get sdk version from viam

--- etc/_update_version_metadata.py
+++ etc/_update_version_metadata.py
@@ -1,30 +1,20 @@
 import re
 import sys
 
-from typing import Literal
 
-
-def update_version(file_path: str, version_type: Literal["api", "sdk"], new_version: str):
+def update_version(file_path: str, new_version: str):
     with open(file_path, "r") as file:
         data = file.read()
 
-    if version_type.lower() == "api":
-        version = r'(API_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"'
-    elif version_type.lower() == "sdk":
-        version = r'(SDK_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"'
-    else:
-        print("version_type must be either 'api' or 'sdk'")
-        sys.exit(1)
-
-    data = re.sub(version, f'\\1"{new_version}"', data)
+    data = re.sub(r'(API_VERSION\s*=\s*)"[0-9]+\.[0-9]+\.[0-9]+"', f'\\1"{new_version}"', data)
 
     with open(file_path, 'w') as file:
         file.write(data)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 etc/update_version.py <file_path> sdk|api <new_version>")
+    if len(sys.argv) != 3:
+        print("Usage: python3 etc/update_version.py <file_path> <new_version>")
         sys.exit(1)
 
-    update_version(sys.argv[1], sys.argv[2], sys.argv[3])  # type: ignore - can't make runtime values Literal
+    update_version(sys.argv[1], sys.argv[2])
--- src/viam/version_metadata.py
+++ src/viam/version_metadata.py
@@ -1,2 +1,4 @@
+import viam
+
 API_VERSION = "0.1.327"
-SDK_VERSION = "0.25.2"
+SDK_VERSION = viam.__version__
