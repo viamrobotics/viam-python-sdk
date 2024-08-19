92f6e7f8c979ea3cf8c5702a06aae67951ab0fe6
randhid <rand@viam.com>
2024-08-19 19:04:44 -0400

    remove status analog names caller

--- src/viam/components/board/__init__.py
+++ src/viam/components/board/__init__.py
@@ -13,18 +13,7 @@ __all__ = ["Board"]
 
 
 async def create_status(component: Board) -> Status:
-    (analog_names, digital_interrupt_names) = await asyncio.gather(component.analog_names(), component.digital_interrupt_names())
     analogs, digital_interrupts = {}, {}
-    for x in analog_names:
-        analog = await component.analog_by_name(x)
-        read = await analog.read()
-        analogs[x] = read.value
-
-    for y in digital_interrupt_names:
-        digital_interrupt = await component.digital_interrupt_by_name(y)
-        val = await digital_interrupt.value()
-        digital_interrupts[y] = val
-
     s = BoardStatus(analogs=analogs, digital_interrupts=digital_interrupts)
     return Status(name=Board.get_resource_name(component.name), status=message_to_struct(s))
 
