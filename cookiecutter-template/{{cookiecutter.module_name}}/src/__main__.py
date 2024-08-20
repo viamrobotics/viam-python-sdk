9e2f62d3fb825c650d62ee902b86ae27c612e7a4
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-08-20 10:12:19 -0400

    change mains

--- cookiecutter-template/{{cookiecutter.module_name}}/src/__main__.py
+++ cookiecutter-template/{{cookiecutter.module_name}}/src/__main__.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python3
+import os
 
-import asyncio
-from viam.module.module import Module
+from cookiecutter.main import cookiecutter
+from importlib.metadata import PackageNotFoundError, version
+
+
+__version__ = "0.1.0"
+try:
+    __version__ = version("viam-sdk")
+except PackageNotFoundError:
+    pass
 
 if __name__ == "__main__":
-    """This function will run your module and make it available to the machine."""
-    asyncio.run(Module.run_from_registry())
+    cookiecutter(
+        f"{os.path.dirname(__file__)}",
+        extra_context={"__generator_version": __version__},
+    )
