a1025f1c68f8310d270c7b7dd4b2719a9c86c105
Naveed Jooma <naveed@viam.com>
2024-09-23 14:26:27 -0400

    Update minimum protobuf version

--- etc/postinstall.sh
+++ etc/postinstall.sh
@@ -1,4 +1,3 @@
-poetry run pip install -U protobuf
 rm -rf src/viam/rpc/libviam_rust_utils.* || true
 if [ "$(uname)" == "Linux" ]; then
     curl -sL -o src/viam/rpc/libviam_rust_utils.so https://github.com/viamrobotics/rust-utils/releases/latest/download/libviam_rust_utils-linux_$(uname -m).so
