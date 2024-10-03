c19134babdae4b240c6c8189f9149ccad89827b3
Sierra Guequierre <sguequierre@gmail.com>
2024-10-03 14:45:54 -0400

    shorten line

--- src/viam/robot/client.py
+++ src/viam/robot/client.py
@@ -759,7 +759,8 @@ class RobotClient:
         queries: List[DiscoveryQuery],
     ) -> List[Discovery]:
         """
-        Get a list of discovered potential component configurations for webcam cameras, for example listing different supported resolutions. Returns module names for modules.
+        Get a list of discovered potential component configurations for webcam cameras, for example listing different supported resolutions.
+        Returns module names for modules.
 
         ::
 
