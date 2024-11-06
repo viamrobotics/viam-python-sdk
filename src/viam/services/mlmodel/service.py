b630e055473e2a38531a553ba89965d11266791c
Naveed Jooma <naveed@viam.com>
2024-11-06 11:47:23 -0500

    Add extra params to MLModel (#783)

--- src/viam/services/mlmodel/service.py
+++ src/viam/services/mlmodel/service.py
@@ -3,6 +3,7 @@ from grpclib.server import Stream
 from viam.proto.service.mlmodel import InferRequest, InferResponse, MetadataRequest, MetadataResponse, MLModelServiceBase
 from viam.resource.rpc_service_base import ResourceRPCServiceBase
 from viam.services.mlmodel.utils import flat_tensors_to_ndarrays, ndarrays_to_flat_tensors
+from viam.utils import struct_to_dict
 
 from .mlmodel import MLModel
 
@@ -19,8 +20,9 @@ class MLModelRPCService(MLModelServiceBase, ResourceRPCServiceBase):
         assert request is not None
         name = request.name
         mlmodel = self.get_resource(name)
+        extra = struct_to_dict(request.extra)
         timeout = stream.deadline.time_remaining() if stream.deadline else None
-        output_tensors = await mlmodel.infer(input_tensors=flat_tensors_to_ndarrays(request.input_tensors), timeout=timeout)
+        output_tensors = await mlmodel.infer(input_tensors=flat_tensors_to_ndarrays(request.input_tensors), extra=extra, timeout=timeout)
         response = InferResponse(output_tensors=ndarrays_to_flat_tensors(output_tensors))
         await stream.send_message(response)
 
@@ -29,7 +31,8 @@ class MLModelRPCService(MLModelServiceBase, ResourceRPCServiceBase):
         assert request is not None
         name = request.name
         mlmodel = self.get_resource(name)
+        extra = struct_to_dict(request.extra)
         timeout = stream.deadline.time_remaining() if stream.deadline else None
-        metadata = await mlmodel.metadata(timeout=timeout)
+        metadata = await mlmodel.metadata(extra=extra, timeout=timeout)
         response = MetadataResponse(metadata=metadata)
         await stream.send_message(response)
