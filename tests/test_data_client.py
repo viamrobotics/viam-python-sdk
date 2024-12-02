488806e45c97d4566603eb4a63b5d9e7abc7b3ff
gloriacai01 <gloriacai101@gmail.com>
2024-12-02 13:24:23 -0500

    lint

--- tests/test_data_client.py
+++ tests/test_data_client.py
@@ -169,7 +169,7 @@ class TestClient:
             client = DataClient(channel, DATA_SERVICE_METADATA)
             time = datetime(2024, 12, 25)
             response = await client.get_latest_tabular_data(PART_ID, COMPONENT_NAME, COMPONENT_TYPE, METHOD)
-            assert response != None
+            assert response is not None
             time_captured, time_synced, payload = response
             assert service.part_id == PART_ID
             assert service.resource_name == COMPONENT_NAME
