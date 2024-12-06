035bc7667301cb1b9315949cdc12223919e36323
viambot <viambot@users.noreply.github.com>
2024-12-06 14:14:05 +0000

    [WORKFLOW] Updating protos from viamrobotics/api, commit: 51e7eb083dec180f8dfa754acbc9ad71d1928284

--- tests/mocks/services.py
+++ tests/mocks/services.py
@@ -1011,8 +1011,9 @@ class MockData(UnimplementedDataServiceBase):
         self.resource_subtype = request.resource_subtype
         self.method_name = request.method_name
         timestamp = datetime_to_timestamp(datetime(2024, 12, 25))
-        data=dict_to_struct(self.tabular_response[0].data)
+        data = dict_to_struct(self.tabular_response[0].data)
         await stream.send_message(GetLatestTabularDataResponse(time_captured=timestamp, time_synced=timestamp, payload=data))
+
 
 class MockDataset(DatasetServiceBase):
     def __init__(self, create_response: str, datasets_response: Sequence[Dataset]):
