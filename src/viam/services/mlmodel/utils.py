f66b367f565924c91c721cbe1b4424eb4db84389
Ethan <ethan@viam.com>
2024-12-20 08:56:43 -0500

    RSDK-9590 - upgrade numpy (#805)

--- src/viam/services/mlmodel/utils.py
+++ src/viam/services/mlmodel/utils.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from packaging.version import Version
 
 import numpy as np
 from numpy.typing import NDArray
@@ -37,7 +38,16 @@ def flat_tensors_to_ndarrays(flat_tensors: FlatTensors) -> Dict[str, NDArray]:
         """Takes flat data (protobuf RepeatedScalarFieldContainer | bytes) to output an ndarray
         of appropriate dtype and shape"""
         make_array = np.frombuffer if dtype == np.int8 or dtype == np.uint8 else np.array
-        return make_array(flat_data, dtype).reshape(shape)
+        # As per proto, int16 and uint16 are stored as uint32. As of numpy v2, this creates
+        # some strange interactions with negative values for int16. Specifically, we end up
+        # trying to create an np.Int16 value with an out of bounds int due to rollover.
+        # Creating our array as a uint32 array initially and then casting to int16 solves this.
+        if Version(np.__version__) >= Version("2") and dtype == np.int16:
+            arr = np.astype(make_array(flat_data, np.uint32), np.int16) # pyright: ignore [reportAttributeAccessIssue]
+
+        else:
+            arr = make_array(flat_data, dtype)
+        return arr.reshape(shape)
 
     ndarrays: Dict[str, NDArray] = dict()
     for name, flat_tensor in flat_tensors.tensors.items():
