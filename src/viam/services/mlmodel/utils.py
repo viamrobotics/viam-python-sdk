4b2ab95ca5f7761bf0eb513b01f505de8d302461
Ethan Rodkin <ethan@viam.com>
2024-12-20 08:28:22 -0500

    pr comments

--- src/viam/services/mlmodel/utils.py
+++ src/viam/services/mlmodel/utils.py
@@ -1,6 +1,5 @@
-import sys
-
 from typing import Dict
+from packaging.version import Version
 
 import numpy as np
 from numpy.typing import NDArray
@@ -43,7 +42,7 @@ def flat_tensors_to_ndarrays(flat_tensors: FlatTensors) -> Dict[str, NDArray]:
         # some strange interactions with negative values for int16. Specifically, we end up
         # trying to create an np.Int16 value with an out of bounds int due to rollover.
         # Creating our array as a uint32 array initially and then casting to int16 solves this.
-        if sys.version_info >= (3, 13) and dtype == np.int16:
+        if Version(np.__version__) >= Version("2") and dtype == np.int16:
             arr = np.astype(make_array(flat_data, np.uint32), np.int16)
         else:
             arr = make_array(flat_data, dtype)
