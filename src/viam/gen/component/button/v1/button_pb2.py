af17fd1723c54d60adcdd2d0efeb5f59d54f9e00
viambot <viambot@users.noreply.github.com>
2025-01-27 17:11:47 +0000

    [WORKFLOW] Updating protos from viamrobotics/api, commit: bdf523364a77f1a6ee559303fcff2cc02a1006e2

---
+++ src/viam/gen/component/button/v1/button_pb2.py
@@ -0,0 +1,28 @@
+"""Generated protocol buffer code."""
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
+from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 29, 1, '', 'component/button/v1/button.proto')
+_sym_db = _symbol_database.Default()
+from ....common.v1 import common_pb2 as common_dot_v1_dot_common__pb2
+from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
+from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n component/button/v1/button.proto\x12\x18viam.component.button.v1\x1a\x16common/v1/common.proto\x1a\x1cgoogle/api/annotations.proto\x1a\x1cgoogle/protobuf/struct.proto"P\n\x0bPushRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12-\n\x05extra\x18c \x01(\x0b2\x17.google.protobuf.StructR\x05extra"\x0e\n\x0cPushResponse2\xa6\x02\n\rButtonService\x12\x88\x01\n\x04Push\x12%.viam.component.button.v1.PushRequest\x1a&.viam.component.button.v1.PushResponse"1\x82\xd3\xe4\x93\x02+\x1a)/viam/api/v1/component/button/{name}/push\x12\x89\x01\n\tDoCommand\x12 .viam.common.v1.DoCommandRequest\x1a!.viam.common.v1.DoCommandResponse"7\x82\xd3\xe4\x93\x021"//viam/api/v1/component/button/{name}/do_commandBC\n\x1ccom.viam.component.button.v1Z#go.viam.com/api/component/button/v1b\x06proto3')
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'component.button.v1.button_pb2', _globals)
+if not _descriptor._USE_C_DESCRIPTORS:
+    _globals['DESCRIPTOR']._loaded_options = None
+    _globals['DESCRIPTOR']._serialized_options = b'\n\x1ccom.viam.component.button.v1Z#go.viam.com/api/component/button/v1'
+    _globals['_BUTTONSERVICE'].methods_by_name['Push']._loaded_options = None
+    _globals['_BUTTONSERVICE'].methods_by_name['Push']._serialized_options = b'\x82\xd3\xe4\x93\x02+\x1a)/viam/api/v1/component/button/{name}/push'
+    _globals['_BUTTONSERVICE'].methods_by_name['DoCommand']._loaded_options = None
+    _globals['_BUTTONSERVICE'].methods_by_name['DoCommand']._serialized_options = b'\x82\xd3\xe4\x93\x021"//viam/api/v1/component/button/{name}/do_command'
+    _globals['_PUSHREQUEST']._serialized_start = 146
+    _globals['_PUSHREQUEST']._serialized_end = 226
+    _globals['_PUSHRESPONSE']._serialized_start = 228
+    _globals['_PUSHRESPONSE']._serialized_end = 242
+    _globals['_BUTTONSERVICE']._serialized_start = 245
+    _globals['_BUTTONSERVICE']._serialized_end = 539
---
+++ src/viam/gen/component/switch/v1/switch_pb2.py
@@ -0,0 +1,40 @@
+"""Generated protocol buffer code."""
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
+from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 29, 1, '', 'component/switch/v1/switch.proto')
+_sym_db = _symbol_database.Default()
+from ....common.v1 import common_pb2 as common_dot_v1_dot_common__pb2
+from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
+from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n component/switch/v1/switch.proto\x12\x18viam.component.switch.v1\x1a\x16common/v1/common.proto\x1a\x1cgoogle/api/annotations.proto\x1a\x1cgoogle/protobuf/struct.proto"s\n\x12SetPositionRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12\x1a\n\x08position\x18\x02 \x01(\rR\x08position\x12-\n\x05extra\x18c \x01(\x0b2\x17.google.protobuf.StructR\x05extra"\x15\n\x13SetPositionResponse"W\n\x12GetPositionRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12-\n\x05extra\x18c \x01(\x0b2\x17.google.protobuf.StructR\x05extra"1\n\x13GetPositionResponse\x12\x1a\n\x08position\x18\x01 \x01(\rR\x08position"`\n\x1bGetNumberOfPositionsRequest\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12-\n\x05extra\x18c \x01(\x0b2\x17.google.protobuf.StructR\x05extra"N\n\x1cGetNumberOfPositionsResponse\x12.\n\x13number_of_positions\x18\x01 \x01(\rR\x11numberOfPositions2\xb9\x05\n\rSwitchService\x12\xa5\x01\n\x0bSetPosition\x12,.viam.component.switch.v1.SetPositionRequest\x1a-.viam.component.switch.v1.SetPositionResponse"9\x82\xd3\xe4\x93\x023\x1a1/viam/api/v1/component/switch/{name}/set_position\x12\xa5\x01\n\x0bGetPosition\x12,.viam.component.switch.v1.GetPositionRequest\x1a-.viam.component.switch.v1.GetPositionResponse"9\x82\xd3\xe4\x93\x023\x1a1/viam/api/v1/component/switch/{name}/get_position\x12\xcb\x01\n\x14GetNumberOfPositions\x125.viam.component.switch.v1.GetNumberOfPositionsRequest\x1a6.viam.component.switch.v1.GetNumberOfPositionsResponse"D\x82\xd3\xe4\x93\x02>\x1a</viam/api/v1/component/switch/{name}/get_number_of_positions\x12\x89\x01\n\tDoCommand\x12 .viam.common.v1.DoCommandRequest\x1a!.viam.common.v1.DoCommandResponse"7\x82\xd3\xe4\x93\x021"//viam/api/v1/component/switch/{name}/do_commandBC\n\x1ccom.viam.component.switch.v1Z#go.viam.com/api/component/switch/v1b\x06proto3')
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'component.switch.v1.switch_pb2', _globals)
+if not _descriptor._USE_C_DESCRIPTORS:
+    _globals['DESCRIPTOR']._loaded_options = None
+    _globals['DESCRIPTOR']._serialized_options = b'\n\x1ccom.viam.component.switch.v1Z#go.viam.com/api/component/switch/v1'
+    _globals['_SWITCHSERVICE'].methods_by_name['SetPosition']._loaded_options = None
+    _globals['_SWITCHSERVICE'].methods_by_name['SetPosition']._serialized_options = b'\x82\xd3\xe4\x93\x023\x1a1/viam/api/v1/component/switch/{name}/set_position'
+    _globals['_SWITCHSERVICE'].methods_by_name['GetPosition']._loaded_options = None
+    _globals['_SWITCHSERVICE'].methods_by_name['GetPosition']._serialized_options = b'\x82\xd3\xe4\x93\x023\x1a1/viam/api/v1/component/switch/{name}/get_position'
+    _globals['_SWITCHSERVICE'].methods_by_name['GetNumberOfPositions']._loaded_options = None
+    _globals['_SWITCHSERVICE'].methods_by_name['GetNumberOfPositions']._serialized_options = b'\x82\xd3\xe4\x93\x02>\x1a</viam/api/v1/component/switch/{name}/get_number_of_positions'
+    _globals['_SWITCHSERVICE'].methods_by_name['DoCommand']._loaded_options = None
+    _globals['_SWITCHSERVICE'].methods_by_name['DoCommand']._serialized_options = b'\x82\xd3\xe4\x93\x021"//viam/api/v1/component/switch/{name}/do_command'
+    _globals['_SETPOSITIONREQUEST']._serialized_start = 146
+    _globals['_SETPOSITIONREQUEST']._serialized_end = 261
+    _globals['_SETPOSITIONRESPONSE']._serialized_start = 263
+    _globals['_SETPOSITIONRESPONSE']._serialized_end = 284
+    _globals['_GETPOSITIONREQUEST']._serialized_start = 286
+    _globals['_GETPOSITIONREQUEST']._serialized_end = 373
+    _globals['_GETPOSITIONRESPONSE']._serialized_start = 375
+    _globals['_GETPOSITIONRESPONSE']._serialized_end = 424
+    _globals['_GETNUMBEROFPOSITIONSREQUEST']._serialized_start = 426
+    _globals['_GETNUMBEROFPOSITIONSREQUEST']._serialized_end = 522
+    _globals['_GETNUMBEROFPOSITIONSRESPONSE']._serialized_start = 524
+    _globals['_GETNUMBEROFPOSITIONSRESPONSE']._serialized_end = 602
+    _globals['_SWITCHSERVICE']._serialized_start = 605
+    _globals['_SWITCHSERVICE']._serialized_end = 1302
