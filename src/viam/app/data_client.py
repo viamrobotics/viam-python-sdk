4225190443d0a0272b9b665df3c82fa12b9e21f7
gloriacai01 <gloriacai101@gmail.com>
2025-06-13 10:23:52 -0400

    add enable backfill

--- src/viam/app/data_client.py
+++ src/viam/app/data_client.py
@@ -1892,7 +1892,7 @@ class DataClient:
         organization_id: str,
         name: str,
         mql_binary: List[Dict[str, Any]],
-        schedule: str,
+        schedule: str, enable_backfill: bool,
         data_source_type: TabularDataSourceType.ValueType = TabularDataSourceType.TABULAR_DATA_SOURCE_TYPE_STANDARD,
     ) -> str:
         """Create a new data pipeline.
@@ -1914,6 +1914,7 @@ class DataClient:
             mql_binary (List[Dict[str, Any]]):The MQL pipeline to run, as a list of MongoDB aggregation pipeline stages.
             schedule (str): A cron expression representing the expected execution schedule in UTC (note this also
                 defines the input time window; an hourly schedule would process 1 hour of data at a time).
+            enable_backfill (bool):  When true, pipeline runs will be scheduled for the organization's past data.
             data_source_type (TabularDataSourceType): The type of data source to use for the pipeline.
                 Defaults to TabularDataSourceType.TABULAR_DATA_SOURCE_TYPE_STANDARD.
 
@@ -1922,7 +1923,7 @@ class DataClient:
         """
         binary: List[bytes] = [bson.encode(query) for query in mql_binary]
         request = CreateDataPipelineRequest(
-            organization_id=organization_id, name=name, mql_binary=binary, schedule=schedule, data_source_type=data_source_type
+            organization_id=organization_id, name=name, mql_binary=binary, schedule=schedule, enable_backfill=enable_backfill, data_source_type=data_source_type
         )
         response: CreateDataPipelineResponse = await self._data_pipelines_client.CreateDataPipeline(request, metadata=self._metadata)
         return response.id
