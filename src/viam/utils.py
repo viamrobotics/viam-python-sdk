d861196a2661588e743ef6e4438e08fe28f34f3e
Emerald <emerald.zhang@viam.com>
2025-07-07 15:04:01 -0400

    unused ipmort

--- src/viam/utils.py
+++ src/viam/utils.py
@@ -6,7 +6,6 @@ import threading
 from datetime import datetime
 from typing import Any, Callable, Dict, List, Mapping, Optional, SupportsBytes, SupportsFloat, Type, TypeVar, Union
 
-from google.protobuf import any_pb2
 from google.protobuf.json_format import MessageToDict, ParseDict
 from google.protobuf.message import Message
 from google.protobuf.struct_pb2 import ListValue, Struct, Value
