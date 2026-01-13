a6a430c90785765f1e8e5c111216bbfe4f7f1cb2
Naveed Jooma <naveed@viam.com>
2026-01-13 14:00:07 -0500

    [RSDK-13016] Add switch labels (#1066)

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -1,62 +1,42 @@
+from typing import cast
+from unittest.mock import AsyncMock
+
 import pytest
 from grpclib.testing import ChannelFor
 
-from viam.components.switch import SwitchClient
+from viam.components.switch import Switch, SwitchClient
 from viam.components.switch.service import SwitchRPCService
 from viam.gen.component.switch.v1.switch_pb2 import (
     GetNumberOfPositionsRequest,
-    GetNumberOfPositionsResponse,
     GetPositionRequest,
-    GetPositionResponse,
     SetPositionRequest,
 )
 from viam.proto.common import (
     DoCommandRequest,
-    DoCommandResponse,
 )
 from viam.proto.component.switch import SwitchServiceStub
 from viam.resource.manager import ResourceManager
-from viam.utils import dict_to_struct, struct_to_dict
+from viam.resource.rpc_client_base import ResourceRPCClientBase
+from viam.utils import dict_to_struct
 
 from . import loose_approx
-from .mocks.components import MockSwitch
+from .mocks import create_mock_subclass
 
 DEFAULT_POSITION = 0
-DEFAULT_NUMBER_OF_POSITIONS = 0
-EXTRA_PARAMS = {"foo": "bar", "baz": [1, 2, 3]}
+DEFAULT_NUMBER_OF_POSITIONS = 3
+DEFAULT_LABELS = ["a", "b", "c"]
+DEFAULT_EXTRA = {"foo": "bar", "baz": [1, 2, 3]}
+EXTRA_PARAMS_STRUCT = dict_to_struct(DEFAULT_EXTRA)
+DEFAULT_TIMEOUT = 1.82
+DEFAULT_TIMEOUT_APPROX = loose_approx(DEFAULT_TIMEOUT)
+DEFAULT_METADATA = ResourceRPCClientBase.Metadata()
+DEFAULT_METADATA.enable_debug_logging
 
 
 @pytest.fixture(scope="function")
-def switch() -> MockSwitch:
-    return MockSwitch(name="sensor", number_of_positions=DEFAULT_NUMBER_OF_POSITIONS, position=DEFAULT_POSITION)
-
-
-class TestSwitch:
-    pos = 2
-
-    async def test_set_position(self, switch):
-        await switch.set_position(self.pos, timeout=1.23, extra=EXTRA_PARAMS)
-        assert switch.position == self.pos
-        assert switch.timeout == loose_approx(1.23)
-        assert switch.extra == EXTRA_PARAMS
-
-    async def test_get_position(self, switch):
-        assert switch.extra is None
-        position = await switch.get_position(timeout=2.34, extra=EXTRA_PARAMS)
-        assert position == DEFAULT_NUMBER_OF_POSITIONS
-        assert switch.extra == EXTRA_PARAMS
-        assert switch.timeout == loose_approx(2.34)
-
-    async def test_get_number_of_positions(self, switch):
-        number_of_positions = await switch.get_number_of_positions(timeout=3.45, extra=EXTRA_PARAMS)
-        assert number_of_positions == DEFAULT_NUMBER_OF_POSITIONS
-        assert switch.extra == EXTRA_PARAMS
-        assert switch.timeout == loose_approx(3.45)
-
-    async def test_do(self, switch):
-        command = {"command": "args"}
-        resp = await switch.do_command(command)
-        assert resp == {"command": command}
+def switch() -> Switch:
+    _Switch = create_mock_subclass(Switch)
+    return _Switch(name="switch")
 
 
 @pytest.fixture(scope="function")
@@ -72,79 +52,78 @@ def service(manager) -> SwitchRPCService:
 class TestService:
     pos = 2
 
-    async def test_set_position(self, switch, service):
+    async def test_set_position(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
             client = SwitchServiceStub(channel)
-            request = SetPositionRequest(name=switch.name, position=self.pos, extra=dict_to_struct(EXTRA_PARAMS))
-            assert switch.extra is None
-            await client.SetPosition(request, timeout=1.23)
-            assert switch.position == self.pos
-            assert switch.extra == EXTRA_PARAMS
-            assert switch.timeout == loose_approx(1.23)
+            request = SetPositionRequest(name=switch.name, position=self.pos, extra=dict_to_struct(DEFAULT_EXTRA))
+            await client.SetPosition(request, timeout=DEFAULT_TIMEOUT, metadata=DEFAULT_METADATA.proto)
+            cast(AsyncMock, switch.set_position).assert_called_once_with(
+                position=self.pos, timeout=DEFAULT_TIMEOUT_APPROX, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA.metadata
+            )
 
-    async def test_get_position(self, switch, service):
+    async def test_get_position(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
             client = SwitchServiceStub(channel)
-            request = GetPositionRequest(name=switch.name, extra=dict_to_struct(EXTRA_PARAMS))
-            assert switch.extra is None
-            result: GetPositionResponse = await client.GetPosition(request, timeout=2.34)
-            assert result.position == DEFAULT_POSITION
-            assert switch.extra == EXTRA_PARAMS
-            assert switch.timeout == loose_approx(2.34)
+            request = GetPositionRequest(name=switch.name, extra=dict_to_struct(DEFAULT_EXTRA))
+            await client.GetPosition(request, timeout=DEFAULT_TIMEOUT, metadata=DEFAULT_METADATA.proto)
+            cast(AsyncMock, switch.get_position).assert_called_once_with(
+                timeout=DEFAULT_TIMEOUT_APPROX, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA.metadata
+            )
 
-    async def test_get_number_of_positions(self, switch, service):
+    async def test_get_number_of_positions(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
+            cast(AsyncMock, switch.get_number_of_positions).return_value = (DEFAULT_NUMBER_OF_POSITIONS, DEFAULT_LABELS)
             client = SwitchServiceStub(channel)
-            request = GetNumberOfPositionsRequest(name=switch.name, extra=dict_to_struct(EXTRA_PARAMS))
-            assert switch.extra is None
-            result: GetNumberOfPositionsResponse = await client.GetNumberOfPositions(request, timeout=4.56)
-            assert result.number_of_positions == DEFAULT_NUMBER_OF_POSITIONS
-            assert switch.extra == EXTRA_PARAMS
-            assert switch.timeout == loose_approx(4.56)
+            request = GetNumberOfPositionsRequest(name=switch.name, extra=dict_to_struct(DEFAULT_EXTRA))
+            await client.GetNumberOfPositions(request, timeout=DEFAULT_TIMEOUT, metadata=DEFAULT_METADATA.proto)
+            cast(AsyncMock, switch.get_number_of_positions).assert_called_once_with(
+                timeout=DEFAULT_TIMEOUT_APPROX, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA.metadata
+            )
 
-    async def test_do(self, switch: MockSwitch, service: SwitchRPCService):
+    async def test_do(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
-            client = SwitchServiceStub(channel)
             command = {"command": "args"}
             request = DoCommandRequest(name=switch.name, command=dict_to_struct(command))
-            response: DoCommandResponse = await client.DoCommand(request)
-            result = struct_to_dict(response.result)
-            assert result == {"command": command}
+            client = SwitchServiceStub(channel)
+            await client.DoCommand(request, timeout=DEFAULT_TIMEOUT, metadata=DEFAULT_METADATA.proto)
+            cast(AsyncMock, switch.do_command).assert_called_once_with(
+                command=command, timeout=DEFAULT_TIMEOUT_APPROX, metadata=DEFAULT_METADATA.metadata
+            )
 
 
 class TestClient:
     pos = 2
 
-    async def test_set_position(self, switch, service):
+    async def test_set_position(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
             client = SwitchClient(switch.name, channel)
-            assert switch.extra is None
-            await client.set_position(position=self.pos, timeout=3.45, extra=EXTRA_PARAMS)
-            assert switch.position == self.pos
-            assert switch.extra == EXTRA_PARAMS
-            assert switch.timeout == loose_approx(3.45)
+            await client.set_position(position=self.pos, timeout=DEFAULT_TIMEOUT, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA)
+            cast(AsyncMock, switch.set_position).assert_called_once_with(
+                position=self.pos, timeout=DEFAULT_TIMEOUT_APPROX, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA.metadata
+            )
 
-    async def test_get_position(self, switch, service):
+    async def test_get_position(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
             client = SwitchClient(switch.name, channel)
-            assert switch.extra is None
-            value_position = await client.get_position(timeout=3.45, extra=EXTRA_PARAMS)
-            assert DEFAULT_POSITION == value_position
-            assert switch.extra == EXTRA_PARAMS
-            assert switch.timeout == loose_approx(3.45)
+            await client.get_position(timeout=DEFAULT_TIMEOUT, extra=DEFAULT_EXTRA)
+            cast(AsyncMock, switch.get_position).assert_called_once_with(
+                timeout=DEFAULT_TIMEOUT_APPROX, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA.metadata
+            )
 
-    async def test_get_number_of_positions(self, switch, service):
+    async def test_get_number_of_positions(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
+            cast(AsyncMock, switch.get_number_of_positions).return_value = (DEFAULT_NUMBER_OF_POSITIONS, DEFAULT_LABELS)
             client = SwitchClient(switch.name, channel)
-            assert switch.extra is None
-            value_number_of_positions = await client.get_number_of_positions(timeout=3.45, extra=EXTRA_PARAMS)
-            assert DEFAULT_NUMBER_OF_POSITIONS == value_number_of_positions
-            assert switch.extra == EXTRA_PARAMS
-            assert switch.timeout == loose_approx(3.45)
+            await client.get_number_of_positions(timeout=DEFAULT_TIMEOUT, extra=DEFAULT_EXTRA)
+            cast(AsyncMock, switch.get_number_of_positions).assert_called_once_with(
+                timeout=DEFAULT_TIMEOUT_APPROX, extra=DEFAULT_EXTRA, metadata=DEFAULT_METADATA.metadata
+            )
 
-    async def test_do(self, switch, manager, service):
+    async def test_do(self, switch: Switch, service: SwitchRPCService):
         async with ChannelFor([service]) as channel:
             client = SwitchClient(switch.name, channel)
             command = {"command": "args"}
-            resp = await client.do_command(command)
-            assert resp == {"command": command}
+            await client.do_command(command, timeout=DEFAULT_TIMEOUT, metadata=DEFAULT_METADATA)
+            cast(AsyncMock, switch.do_command).assert_called_once_with(
+                command=command, timeout=DEFAULT_TIMEOUT_APPROX, metadata=DEFAULT_METADATA.metadata
+            )
