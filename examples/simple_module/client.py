c5df4894fe51c1160945f276d9734064da6c3c75
Allison Chiang <allisonsc123@gmail.com>
2026-03-05 17:59:06 -0500

    exampleswork

--- examples/simple_module/client.py
+++ examples/simple_module/client.py
@@ -6,14 +6,16 @@ from viam.rpc.dial import DialOptions
 from viam.components.sensor import Sensor
 
 
-async def connect(machine_url: str, api_key_id: str, api_key: str):
-    # opts = RobotClient.Options.with_api_key(api_key=api_key_id, api_key_id=api_key)
-    opts = RobotClient.Options(dial_options=DialOptions(insecure=True, disable_webrtc=True))
+async def connect(machine_url: str, api_key_id: str, api_key: str, local: bool = False):
+    if local:
+        opts = RobotClient.Options(dial_options=DialOptions(insecure=True, disable_webrtc=True))
+    else:
+        opts = RobotClient.Options.with_api_key(api_key=api_key, api_key_id=api_key_id)
     return await RobotClient.at_address(machine_url, opts)
 
 
-async def main(machine_url: str,api_key_id: str, api_key: str):
-    robot = await connect(machine_url, api_key_id, api_key)
+async def main(machine_url: str, api_key_id: str, api_key: str, local: bool = False):
+    robot = await connect(machine_url, api_key_id, api_key, local)
 
     print("Resources:")
     print(robot.resource_names)
@@ -30,6 +32,12 @@ async def main(machine_url: str,api_key_id: str, api_key: str):
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    if len(args) < 3:
-        raise ValueError("Usage: python client.py <machine_url> <api_key_id> <api_key>")
-    asyncio.run(main(args[0], args[1], args[2]))
+    if len(args) >= 3:
+        asyncio.run(main(args[0], args[1], args[2], local=True))
+    else:
+        # Update these values with your app.viam.com credentials
+        asyncio.run(main(
+            "<your machine url>",
+            "<your api key id>",
+            "<your api key>",
+        ))
