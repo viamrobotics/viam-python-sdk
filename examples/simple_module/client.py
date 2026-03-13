f36fcbaf470cf4300d4ba477175ee35fc109310a
Allison Chiang <83091926+allisonschiang@users.noreply.github.com>
2026-03-13 14:36:33 -0400

    RSDK-13254: python examples testing (#1148)

--- examples/simple_module/client.py
+++ examples/simple_module/client.py
@@ -1,16 +1,21 @@
 import asyncio
+import sys
 
 from viam.robot.client import RobotClient
+from viam.rpc.dial import DialOptions
 from viam.components.sensor import Sensor
 
 
-async def connect():
-    opts = RobotClient.Options.with_api_key(api_key="<your api key here>", api_key_id="<your api key ID here>")
-    return await RobotClient.at_address("<your robot uri here>", opts)
+async def connect(machine_url: str, api_key_id: str, api_key: str, local: bool = False):
+    if local:
+        opts = RobotClient.Options(dial_options=DialOptions(insecure=True, disable_webrtc=True))
+    else:
+        opts = RobotClient.Options.with_api_key(api_key=api_key, api_key_id=api_key_id)
+    return await RobotClient.at_address(machine_url, opts)
 
 
-async def main():
-    robot = await connect()
+async def main(machine_url: str, api_key_id: str, api_key: str, local: bool = False):
+    robot = await connect(machine_url, api_key_id, api_key, local)
 
     print("Resources:")
     print(robot.resource_names)
@@ -26,4 +31,13 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    args = sys.argv[1:]
+    if len(args) >= 4:
+        asyncio.run(main(args[0], args[1], args[2], local=args[3] == "True"))
+    else:
+        # Update these values with your app.viam.com credentials
+        asyncio.run(main(
+            "<your machine url>",
+            "<your api key id>",
+            "<your api key>",
+        ))
