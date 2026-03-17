9da487d4b6ab49a62dd079dd0121555a42981320
Alden Hsu <alden.hsu@viam.com>
2026-03-17 14:52:43 -0400

    DeprecationWarning when subclassing Reconfigurable

--- src/viam/module/types.py
+++ src/viam/module/types.py
@@ -1,9 +1,12 @@
 from typing import Any, Mapping, Optional, Protocol, runtime_checkable
+import warnings
 
 
 @runtime_checkable
 class Reconfigurable(Protocol):
     """The Reconfigurable protocol defines the requirements for making a resource Reconfigurable"""
+    def __init_subclass__(*args, **kwargs):
+        warnings.warn("Reconfigure is deprecated, and resources will always rebuild. It is not necessary to implement Reconfigurable.", DeprecationWarning, stacklevel=2)
 
 
 @runtime_checkable
